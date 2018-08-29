a12bba93ff747c1ba37f3717af5c3667cf9feba8
Abdullah Sanver <a_sanver@hotmail.com>
2018-08-29 03:32:26 +0100

    IPMatching added.

---
+++ src/main/java/com/sanver/basics/regex/IPMatching.java
@@ -0,0 +1,20 @@
+package com.sanver.basics.regex;
+
+import java.util.Scanner;
+
+public class IPMatching {
+    public static void main(String... args) {
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+        String pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+
+                System.out.print("Ip: ");
+                String text = scanner.nextLine();
+                if (text.trim().isEmpty())
+                    return;
+                System.out.println(text.matches(pattern) ? "Valid IP" : "Not valid IP");
+            }
+        }
+    }
+}
