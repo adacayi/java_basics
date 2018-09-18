d3b37489758662b605d5bb5368eb46f58ea5a285
Abdullah Sanver <a_sanver@hotmail.com>
2018-09-18 14:37:50 +0100

    In RemovingDuplicates, code is modified to remove all duplicate words so every word is used only once. The previous example only removed duplicates when they were adjacent to each other. Comment added to AggressiveMatching.

--- src/main/java/com/sanver/basics/regex/AggressiveMatching.java
+++ src/main/java/com/sanver/basics/regex/AggressiveMatching.java
@@ -7,7 +7,8 @@ public class AggressiveMatching {
 
     public static void main(String... args) {
         String message = "abdullah.sanver.sancode.co.uk~~~";
-        String patternString = "[\\w.]+\\.\\w{2,6}";
+        String patternString = "[\\w.]+\\.\\w{2,6}"; // . inside bracket means . literally.
+        // But outside [] it means any character.
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(message);
         System.out.println(message);
--- src/main/java/com/sanver/basics/regex/RemovingDuplicates.java
+++ src/main/java/com/sanver/basics/regex/RemovingDuplicates.java
@@ -5,16 +5,24 @@ import java.util.regex.Pattern;
 
 public class RemovingDuplicates {
     public static void main(String... args) {
-        String message = "Goodbye bye Bye in iN inthe";
-        String regex = "(\\b\\w+)(\\W+\\1\\b)+";// \b is used to mean beginning or end of a word.
+        boolean isReplaced;
+        //This application is to remove any duplicate words.
+        String message = "Goodbye bye Bye in iN in the end bye";
+        String regex = "(\\b\\w+)(.*)(\\1\\b\\s*)+";// \b is used to mean beginning or end of a word.
         // \1 is used to show the first captured group
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(message);
         System.out.println(message);
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-            message = message.replaceAll(matcher.group(), matcher.group(1));
-            System.out.println(message);
-        }
+
+        do {
+            Matcher matcher = pattern.matcher(message);
+            isReplaced = false;
+            System.out.println("\n---\n");
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+                message = message.replaceAll(matcher.group(), matcher.group(1) + matcher.group(2));
+                System.out.println(message);
+                isReplaced = true;
+            }
+        } while (isReplaced);
     }
 }
