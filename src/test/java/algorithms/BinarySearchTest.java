0130070714a7f3702ad359a722125596701ce1ef
Abdullah Sanver <a_sanver@hotmail.com>
2018-09-28 17:14:27 +0100

    searchFirst and searchLast methods added to BinarySearch.

--- src/test/java/algorithms/BinarySearchTest.java
+++ src/test/java/algorithms/BinarySearchTest.java
@@ -4,8 +4,6 @@ import com.sanver.basics.algorithms.BinarySearch;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -57,6 +55,54 @@ public class BinarySearchTest {
         int[] array = {0, 3, 4, 5};
         int expected = -3;
         int actual = binarySearch.search(array, 2, 4, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return4_When_234446_Last_4() {
+        int[] array = {2, 3, 4, 4, 4, 6};
+        int expected = 4;
+        int actual = binarySearch.searchLast(array, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_ReturnMinus6_When_234446_Last_5() {
+        int[] array = {2, 3, 4, 4, 4, 6};
+        int expected = -6;
+        int actual = binarySearch.searchLast(array, 5);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return3_When_234445_InSubArray1_Last_4() {
+        int[] array = {2, 3, 4, 4, 4, 5};
+        int expected = 3;
+        int actual = binarySearch.searchLast(array, 1, 4, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return2_When_234446_First_4() {
+        int[] array = {2, 3, 4, 4, 4, 6};
+        int expected = 2;
+        int actual = binarySearch.searchFirst(array, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_ReturnMinus6_When_234446_First_5() {
+        int[] array = {2, 3, 4, 4, 4, 6};
+        int expected = -6;
+        int actual = binarySearch.searchFirst(array, 5);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return3_When_234445_InSubArray3_6_First_4() {
+        int[] array = {2, 3, 4, 4, 4, 5};
+        int expected = 3;
+        int actual = binarySearch.searchFirst(array, 3, 6, 4);
         assertEquals(expected, actual);
     }
 }
