3d14eda938c495793e8b134b831a11b6803b32a3
sanvera <abdullah.sanver@ofgem.gov.uk>
2019-10-14 14:53:44 +0100

    Adds distinct sample for non primitive arrays

--- src/main/java/com/sanver/basics/stream_api/DistinctSample.java
+++ src/main/java/com/sanver/basics/stream_api/DistinctSample.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class DistinctSample {
 
-	public static void main(String[] args) {
-		int[] numbers = { 3, 2, 5, 6, 3, 3 };
-		System.out.println(Arrays.toString(Arrays.stream(numbers).distinct().toArray()));
-	}
-
+    public static void main(String[] args) {
+        int[] numbers = {3, 2, 5, 6, 3, 3};
+        String[] names = {"Ahmet", "Salih", "Ahmet"};
+        System.out.println(Arrays.toString(Arrays.stream(numbers).distinct().toArray()));
+		System.out.println(Arrays.toString(Arrays.stream(names).distinct().toArray(String[]::new)));
+    }
 }
