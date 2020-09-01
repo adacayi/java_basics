11f86b19e458b29afbf021dee1dce602f34f8227
Abdullah Sanver <abdullah.sanver@xyzreality.com>
2020-09-01 15:04:53 +0100

    Add SHA512 hashing

---
+++ src/main/java/com/sanver/basics/hashing/SHA512HashingWithSalt.java
@@ -0,0 +1,40 @@
+package com.sanver.basics.hashing;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class SHA512HashingWithSalt {
+    public static final String SHA_512 = "SHA-512";
+    private static MessageDigest messageDigest;
+
+    public synchronized static String hash(String message) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        if (messageDigest == null) {
+            try {
+                messageDigest = MessageDigest.getInstance(SHA_512);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        messageDigest.update(salt);
+        var hashedBytes = messageDigest.digest(message.getBytes(StandardCharsets.UTF_8));
+        return String.format("%064x", new BigInteger(1, hashedBytes));
+    }
+
+    public static void main(String... args) {
+        // BCrypt is recommended for hashing, since it is slower to implement, hence more prone to hacking. Spring
+        // uses it.
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter message to be hashed: ");
+            String message = scanner.nextLine();
+            System.out.printf("Hashed form: %s", hash(message));
+        }
+    }
+}
