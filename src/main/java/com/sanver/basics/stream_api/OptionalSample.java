0a2ab186bb3c3591d3c2823b183f8d0662ecb1f0
Abdullah Sanver <abdullah.sanver@valtech.com>
2020-11-05 21:05:50 +0000

    Add map and orElse usage in Optional

--- src/main/java/com/sanver/basics/stream_api/OptionalSample.java
+++ src/main/java/com/sanver/basics/stream_api/OptionalSample.java
@@ -1,34 +1,49 @@
 package com.sanver.basics.stream_api;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
 
 public class OptionalSample {
 
-	public static void main(String[] args) {
-		String value = null;
-		// Optional<String> optional=Optional.of(value);// This will give a runtime
-		// exception since the value is null
-		Optional<String> optional = Optional.ofNullable(value);
-		System.out.println(optional);
-		System.out.println(optional.isPresent());
-		// System.out.println(optional.get()); // This will give a runtime error since
-		// the value is null
-		Optional<Double> doubleOptional = Optional.of(2.3);
-		System.out.println(doubleOptional);
-		System.out.println(doubleOptional.isPresent());
-		System.out.println(doubleOptional.get());
-		doubleOptional = Optional.empty();
-		System.out.println(optional.isPresent());
-		System.out.println(doubleOptional);
-		System.out.println(doubleOptional.orElse(3.2));
-		OptionalDouble optionalDouble = DoubleStream.of(2, 3, 4).average();
-		System.out.println(optionalDouble);
-		System.out.println(optionalDouble.isPresent());
-		System.out.println(optionalDouble.getAsDouble());
-		optionalDouble = OptionalDouble.empty();
-		System.out.println(optionalDouble);
-	}
-
+  public static void main(String[] args) {
+    var names = List.of("abc", "abd", "bcd");
+    var resultA = names
+        .stream()
+        .filter(x -> x.startsWith("a"))
+        .findFirst()
+        .map(x -> "name starting with a " + x)
+        .orElse("na");
+    var resultC = names
+        .stream()
+        .filter(x -> x.startsWith("c"))
+        .findFirst()
+        .map(x -> "name starting with c " + x)
+        .orElse("na");
+		System.out.println(resultA);
+		System.out.println(resultC);
+    String value = null;
+    // Optional<String> optional=Optional.of(value);// This will give a runtime
+    // exception since the value is null
+    Optional<String> optional = Optional.ofNullable(value);
+    System.out.println(optional);
+    System.out.println(optional.isPresent());
+    // System.out.println(optional.get()); // This will give a runtime error since
+    // the value is null
+    Optional<Double> doubleOptional = Optional.of(2.3);
+    System.out.println(doubleOptional);
+    System.out.println(doubleOptional.isPresent());
+    System.out.println(doubleOptional.get());
+    doubleOptional = Optional.empty();
+    System.out.println(optional.isPresent());
+    System.out.println(doubleOptional);
+    System.out.println(doubleOptional.orElse(3.2));
+    OptionalDouble optionalDouble = DoubleStream.of(2, 3, 4).average();
+    System.out.println(optionalDouble);
+    System.out.println(optionalDouble.isPresent());
+    System.out.println(optionalDouble.getAsDouble());
+    optionalDouble = OptionalDouble.empty();
+    System.out.println(optionalDouble);
+  }
 }
