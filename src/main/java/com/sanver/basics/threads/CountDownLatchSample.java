87f54986fa1b63419e8caf5d8d8624a473bd7e13
Abdullah Sanver <abdullah.sanver@valtech.com>
2021-05-06 17:44:43 +0100

    Add CountDownLatchSample

---
+++ src/main/java/com/sanver/basics/threads/CountDownLatchSample.java
@@ -0,0 +1,28 @@
+package com.sanver.basics.threads;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchSample {
+
+  public static void main(String[] args) throws InterruptedException {
+    var count = 3;
+    var countDownLatch = new CountDownLatch(count);
+    var thread1 = new Thread(() -> {
+      try {
+        System.out.println("Count down started with " + countDownLatch.getCount());
+        countDownLatch.await();
+        System.out.println("Count down finished");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    thread1.start();
+
+    for (int i = 0; i < count; i++) {
+      Thread.sleep(3000);
+      countDownLatch.countDown();
+      System.out.println("Count down " + countDownLatch.getCount());
+    }
+  }
+}
