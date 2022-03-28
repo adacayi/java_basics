85ab22ad3afc9ead800ee497f55aedc9b7d0bc29
Abdullah Sanver <abdullah.sanver@valtech.com>
2022-03-28 19:45:43 +0100

    Add comparator working with object with null fields

---
+++ src/main/java/com/sanver/basics/comparing/ComparatorObjectsWithNullFieldsSample.java
@@ -0,0 +1,41 @@
+package com.sanver.basics.comparing;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import lombok.Value;
+
+public class ComparatorObjectsWithNullFieldsSample {
+
+  private static final String PRODUCT_1 = "Product 1";
+  private static final String PRODUCT_2 = "Product 2";
+
+  @Value
+  static class Product {
+
+    String name;
+    int code;
+    String country;
+  }
+
+  public static void main(String[] args) {
+    var product1 = new Product(PRODUCT_1, 3, null);
+    var product2 = new Product(PRODUCT_1, 3, "US");
+    var product3 = new Product(PRODUCT_1, 2, "IN");
+    var product4 = new Product(PRODUCT_2, 2, "IN");
+
+    var list = new ArrayList<>(List.of(product1, product2, product3, product4));
+    var comparatorNullsFirst = Comparator
+        .comparing(Product::getName)
+        .thenComparing(Product::getCode)
+        .thenComparing(Product::getCountry, Comparator.nullsFirst(Comparator.comparing(x -> x)));
+    var comparatorNullsLast = Comparator
+        .comparing(Product::getName)
+        .thenComparing(Product::getCode)
+        .thenComparing(Product::getCountry, Comparator.nullsLast(Comparator.comparing(x -> x)));
+    list.sort(comparatorNullsFirst);
+    System.out.println(list);
+    list.sort(comparatorNullsLast);
+    System.out.println(list);
+  }
+}
