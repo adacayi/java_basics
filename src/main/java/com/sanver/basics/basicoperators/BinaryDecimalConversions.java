f1a215d1d6968d9ae82d8dcf877b9650de7c76ec
Abdullah Sanver <abdullah.sanver@valtech.com>
2023-08-22 14:34:14 +0100

    Add explanation for binary representation of decimal numbers and more examples.

--- src/main/java/com/sanver/basics/basicoperators/BinaryDecimalConversions.java
+++ src/main/java/com/sanver/basics/basicoperators/BinaryDecimalConversions.java
@@ -1,17 +1,28 @@
 package com.sanver.basics.basicoperators;
 
-import static com.sanver.basics.basicoperators.BinaryHelpers.*;
+import static com.sanver.basics.basicoperators.BinaryHelpers.writeBinaryOfInt;
 
 public class BinaryDecimalConversions {
     public static void main(String... args) {
+        // Documentation for how negative numbers are represented in binary form: https://www.geeksforgeeks.org/representation-of-negative-binary-numbers/
+        // In short, we don't just use the first bit to denote sign, because for 0 we have 2 representations and we for example for 8 bits, we can only represent down to -127, and not -128 because of that.
+        // For having a single representation of 0 and be able to represent -128 with 8 bits, for any negative number -x, we represent its binary form as follows:
+        // We take the complement (change 0s to 1s and 1s to 0s) of the positive x and then add 1 (This operation is called 2's complement method).
+        // So for -1, we have 1 in 8 bits as 00000001, we take its complement 11111110 and then add one and get 11111111 as the binary representation of -1.
+        // For -128 in 8 bits though let's represent -127 first and then subtract 1 since we cannot represent 128 with 8 bits (if we want negatives as well).
+        // 127 is 01111111. The complement is 10000000. If we add 1 then we get 10000001 as -127. Now -128 is -1 of that. So it is 10000000 in 8 bits.
         int number = 2;
-        System.out.printf("To write the negative of %d write ~(%d): %d\n", number, number - 1, ~(number - 1));
+        System.out.printf("To write the negative of %d write ~%d + 1 = %d\n", number, number, ~number + 1);
         number = -2;
-        System.out.printf("To write the negative of %d write ~(%d): %d\n", number, number - 1, ~(number - 1));
+        System.out.printf("To write the negative of %d write ~(%d) + 1 = %d\n", number, number, ~number + 1);
         System.out.println();
-        writeBinaryOfInt(0);
-        writeBinaryOfInt(-1);// Negative numbers are written as bitwise complements of (its absolute value -1).
-        // In the case of -1 it is the bitwise of 0.
+        writeBinaryOfInt(0, "%-5d: %s (32 bit representation)", 0);
+        writeBinaryOfInt(0, "%-5d: %s (8 bit representation)", 0, 8);
+        writeBinaryOfInt(-1, "%-5d: %s (32 bit representation)", -1);
+        writeBinaryOfInt(-1, "%-5d: %s (8 bit representation)", -1, 8);
+        writeBinaryOfInt(128, "%-5d: %s (32 bit representation)", 128);
+        writeBinaryOfInt(-128, "%-5d: %s (32 bit representation)", -128);
+        writeBinaryOfInt(-128, "%-5d: %s (8 bit representation)", -128, 8);
         writeBinaryOfInt(1);
         writeBinaryOfInt(-2);
         writeBinaryOfInt(4);
--- src/main/java/com/sanver/basics/basicoperators/BinaryHelpers.java
+++ src/main/java/com/sanver/basics/basicoperators/BinaryHelpers.java
@@ -2,16 +2,21 @@ package com.sanver.basics.basicoperators;
 
 public class BinaryHelpers {
     public static void writeBinaryOfInt(int number, Object... args) {
-        int newNumber = number;
-        String format = "%-5d: %s";
+        var format = "%-5d: %s";
+        var newNumber = number;
+        var numberOfBits = 32;
 
         if (args != null && args.length > 0) {
             format = (String) args[0];
-            if (args.length > 1)
+            if (args.length > 1) {
                 newNumber = (int) args[1];
+            }
+            if (args.length > 2) {
+                numberOfBits = (int) args[2];
+            }
         }
 
-        System.out.printf(format, number, toBinaryString(newNumber));
+        System.out.printf(format, number, toBinaryString(newNumber, numberOfBits));
 
         if (newNumber != number)
             System.out.printf(" Value: %d\n", newNumber);
@@ -19,12 +24,18 @@ public class BinaryHelpers {
             System.out.println();
     }
 
-    public static String toBinaryString(int number) {
+    public static String toBinaryString(int number, int numberOfBits) {
+        if (numberOfBits > 32) {
+            throw new IllegalArgumentException("This can only work up to 32 bit representation");
+        }
+
         StringBuilder result = new StringBuilder();
 
-        for (int i = 31; i >= 0; i--) {
-            result.append(number << (31 - i) >>> 31);
+        for (int i = 0; i < numberOfBits; i++) {
+            result.append(number << (31 - ((numberOfBits - 1) - i)) >>> 31); // This is to first clear out the bits before the ith digit by moving it to the far left and then move it to the far right, so only the ith digit remains as the far most digit and all others are 0.
+            // Remember, even when doing bitwise operations on types like bytes with 8 bit representation, the result will be calculated in 32 bit, hence when moving the digits to far left we need to move it not by 7-i but 31-(7-i).
         }
+
         return result.toString();
     }
 }
