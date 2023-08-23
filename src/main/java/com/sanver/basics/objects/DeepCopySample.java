3a976e40dac8f4ea11cee2e29f46c808d2a3a2a7
Abdullah Sanver <abdullah.sanver@valtech.com>
2023-08-23 13:07:14 +0100

    Add DeepCopySample

---
+++ src/main/java/com/sanver/basics/objects/DeepCopySample.java
@@ -0,0 +1,29 @@
+package com.sanver.basics.objects;
+
+import com.google.gson.Gson;
+import lombok.Data;
+import lombok.NonNull;
+
+public class DeepCopySample {
+    public static void main(String[] args) {
+        var person1 = new Person("Johnny", 16);
+        var person2 = deepCopy(person1);
+        person1.setAge(1);
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T deepCopy(T object) {
+        // The advantage of using Gson instead of ObjectMapper is, it doesn't require a default constructor for the class to be deserialized
+        // https://www.baeldung.com/java-deep-copy
+        var gson = new Gson();
+        return (T) gson.fromJson(gson.toJson(object), object.getClass());
+    }
+
+    @Data
+    static class Person {
+        @NonNull String name;
+        @NonNull int age;
+    }
+}
