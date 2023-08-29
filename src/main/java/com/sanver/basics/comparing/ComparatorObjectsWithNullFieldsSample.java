df389cf6dbba7ee58c161d6caf646e93c50bb49f
Abdullah Sanver <abdullah.sanver@valtech.com>
2023-08-29 17:24:20 +0100

    Add new data to sort to ComparatorObjectsWithNullFieldsSample

--- src/main/java/com/sanver/basics/comparing/ComparatorObjectsWithNullFieldsSample.java
+++ src/main/java/com/sanver/basics/comparing/ComparatorObjectsWithNullFieldsSample.java
@@ -16,15 +16,21 @@ public class ComparatorObjectsWithNullFieldsSample {
     String name;
     int code;
     String country;
+
+    @Override
+    public String toString() {
+      return String.format("[%s, %s, %s]", name, code, country);
+    }
   }
 
   public static void main(String[] args) {
     var product1 = new Product(PRODUCT_1, 3, null);
     var product2 = new Product(PRODUCT_1, 3, "US");
-    var product3 = new Product(PRODUCT_1, 2, "IN");
-    var product4 = new Product(PRODUCT_2, 2, "IN");
+    var product3 = new Product(PRODUCT_1, 3, "IN");
+    var product4 = new Product(PRODUCT_1, 2, "IN");
+    var product5 = new Product(PRODUCT_2, 2, "IN");
 
-    var list = new ArrayList<>(List.of(product1, product2, product3, product4));
+    var list = new ArrayList<>(List.of(product1, product2, product3, product4, product5));
     var comparatorNullsFirst = Comparator
         .comparing(Product::getName)
         .thenComparing(Product::getCode)
