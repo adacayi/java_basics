eaccb3d50b589374f741834df05d14ec1276782c
Abdullah Sanver <abdullah.sanver@valtech.com>
2023-08-30 15:48:37 +0100

    Add displaying values and entries for HashMap in HashMapSample

--- src/main/java/com/sanver/basics/collections/HashMapSample.java
+++ src/main/java/com/sanver/basics/collections/HashMapSample.java
@@ -13,16 +13,17 @@ public class HashMapSample {
         // since the key did not exist before
         System.out.println(hashMap.get("Ahmet"));
         System.out.println(hashMap.get("Salih"));// If there is no such key null is returned.
-        System.out.print("Keys: ");
-        System.out.println(String.join(", ", hashMap.keySet()));
         hashMap.forEach((k, v) -> System.out.printf("Key= %s, Value=%s\n", k, v));
         System.out.println(hashMap.put("Muhammed", "Sanver"));// If a key already exists the old value is returned with
         // put and the value is updated with the new value. Else null is returned.
         hashMap.forEach((k, v) -> System.out.printf("Key= %s, Value=%s\n", k, v));
-        System.out.printf("\nRemoving Ahmet\n");
+        System.out.printf("Keys: %s\n", hashMap.keySet());
+        System.out.printf("Values: %s\n", hashMap.values());
+        System.out.printf("Entries: %s\n", hashMap.entrySet());
+        System.out.print("\nRemoving Ahmet\n");
         System.out.printf("%s", hashMap.remove("Ahmet")); // Remove returns the value of the key. If the key is
         // not present, it will return null
-        System.out.printf("\n");
+        System.out.print("\n");
         hashMap.forEach((k, v) -> System.out.printf("Key= %s, Value=%s\n", k, v));
     }
 }
