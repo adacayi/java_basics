5ec50b636769a28cc051352f7e70f28e4303e22b
Abdullah Sanver <abdullah.sanver@valtech.com>
2024-01-17 15:12:05 +0000

    Add MonoSample

--- src/main/java/com/sanver/basics/reactive/MonoSample.java
+++ src/main/java/com/sanver/basics/reactive/MonoSample.java
@@ -1,17 +1,17 @@
 package com.sanver.basics.reactive;
 
 import org.slf4j.MDC;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 
-public class FluxSample {
+public class MonoSample {
     public static void main(String[] args) {
         var result = new ArrayList<Integer>();
-        MDC.put("userName", "asanver"); // This is also to show that when we subscribe to flux, the same thread is used, hence the same MDC context is used for logs.
+        MDC.put("userName", "asanver"); // This is also to show that when we subscribe to mono, the same thread is used, hence the same MDC context is used for logs.
         System.out.println("Main thread. Thread Id: " + Thread.currentThread().getId());
-        Flux<Integer> flux = Flux.just(1, 2, 3, 4);
-        flux.log().subscribe(x -> {
+        Mono<Integer> mono = Mono.just(4);
+        mono.log().subscribe(x -> {
             result.add(x);
             System.out.printf("%d added to the list. Thread id: %d%n", x, Thread.currentThread().getId());
         }); // Note, logging wouldn't log anything if we didn't subscribe.
