0a5e220eaab2a9025b5f8abb790669e7b9226b1e
Abdullah Sanver <abdullah.sanver@valtech.com>
2024-02-07 17:24:03 +0000

    Add samples for converting to LongStream and DoubleStream in ConvertingObjectStreamsToPrimitiveStreamsSample

--- src/main/java/com/sanver/basics/streamapi/ConvertingObjectStreamsToPrimitiveStreamsSample.java
+++ src/main/java/com/sanver/basics/streamapi/ConvertingObjectStreamsToPrimitiveStreamsSample.java
@@ -7,6 +7,7 @@ public class ConvertingObjectStreamsToPrimitiveStreamsSample {
 	public static void main(String[] args) {
 		String[] numbers = { "3", "1", "2", "5" };
 		Arrays.stream(numbers).mapToInt(Integer::parseInt).average().ifPresent(x -> System.out.println("Average is " + x));
+		Arrays.stream(numbers).mapToLong(Long::parseLong).average().ifPresent(x -> System.out.println("Average is " + x));
+		Arrays.stream(numbers).mapToDouble(Integer::parseInt).average().ifPresent(x -> System.out.println("Average is " + x));
 	}
-
 }
