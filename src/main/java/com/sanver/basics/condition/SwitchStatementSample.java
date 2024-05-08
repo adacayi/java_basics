9746a56ee94891713c6f9d59c790158ad53cd729
Abdullah Sanver <abdullah.sanver@valtech.com>
2024-05-08 17:22:41 +0100

    Add switch samples

---
+++ src/main/java/com/sanver/basics/condition/SwitchStatementSample.java
@@ -0,0 +1,54 @@
+package com.sanver.basics.condition;
+
+import static com.sanver.basics.condition.SwitchStatementSample.Animal.CAT;
+import static com.sanver.basics.condition.SwitchStatementSample.Animal.TIGER;
+
+public class SwitchStatementSample {
+    public static void main(String[] args) {
+        /* The switch statement accepts only the following data types:
+            byte and Byte
+            short and Short
+            int and Integer
+            char and Character
+            enum
+            String
+            Use enhanced switch statement instead after Java 12.
+         */
+        printAnimalType(CAT);
+        var tigerType = getAnimalType(TIGER);
+        System.out.println(tigerType);
+    }
+
+    public static void printAnimalType(Animal animal) {
+        String result;
+        switch (animal) {
+            case DOG:
+            case CAT: // These two cases can be combined into one case as shown in the below getAnimalType() method.
+                result = "domestic animal";
+                break;
+            case TIGER:
+                result = "wild animal";
+                break;
+            default:
+                result = "unknown animal";
+                break;
+        }
+
+        System.out.println(result);
+    }
+
+    public static String getAnimalType(Animal animal) {
+        switch (animal) {
+            case DOG, CAT:
+                return "domestic animal";
+            case TIGER:
+                return "wild animal";
+            default:
+                return "unknown animal";
+        }
+    }
+
+    public enum Animal {
+        DOG, CAT, TIGER
+    }
+}
