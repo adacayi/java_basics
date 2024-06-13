cd9d70794c069dca7b996b8eb10373eff22068c0
Abdullah Sanver <a_sanver@hotmail.com>
2024-06-13 19:11:54 +0100

    Add difference to HashSetSample as well

--- src/main/java/com/sanver/basics/collections/HashSetSample.java
+++ src/main/java/com/sanver/basics/collections/HashSetSample.java
@@ -4,15 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HashSetSample {
-
     public static void main(String[] args) {
         Set<String> firstSet = new HashSet<>();
         Set<String> secondSet = new HashSet<>();
         Set<String> union;
         Set<String> intersection;
+        Set<String> differenceFirstSet;
 
         firstSet.add("Ahmet");
-        System.out.printf("Result of adding an element for the second time: %s\n", firstSet.add("Ahmet"));
+        System.out.printf("Result of adding an element for the second time: %s%n", firstSet.add("Ahmet"));
         firstSet.add("Muhammed");
         secondSet.add("Mustafa");
         secondSet.add("Muhammed");
@@ -20,12 +20,14 @@ public class HashSetSample {
         union.addAll(secondSet);
         intersection = new HashSet<>(firstSet);
         intersection.retainAll(secondSet);
+        differenceFirstSet = new HashSet<>(firstSet);
+        differenceFirstSet.removeAll(secondSet);
 
         System.out.println(firstSet.contains("Ahmet"));
         System.out.println(String.join(", ", firstSet));
         System.out.println(secondSet);
         System.out.println(union);
         System.out.println(intersection);
+        System.out.println(differenceFirstSet);
     }
-
 }
