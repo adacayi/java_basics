eb272609dacaa830ad7476e7af8b0b813182fd52
Abdullah Sanver <a_sanver@hotmail.com>
2024-08-08 18:50:43 +0100

    Refactor SimpleThreadSample

--- src/main/java/com/sanver/basics/threads/SimpleThreadSample.java
+++ src/main/java/com/sanver/basics/threads/SimpleThreadSample.java
@@ -2,38 +2,38 @@ package com.sanver.basics.threads;
 
 public class SimpleThreadSample {
 
-	class Increment extends Thread {
-		int[] value;
+	private static int value = 0;
+
+	static class Increment extends Thread {
 		int incrementValue;
 
-		public Increment(int[] value, int incrementValue) {
-			this.value = value;
+		public Increment(int incrementValue) {
 			this.incrementValue = incrementValue;
 		}
 
+		@Override
 		public void run() {
-			for (int i = 0; i < incrementValue; i++, value[0]++)
-				;
+			for (int i = 0; i < incrementValue; i++){
+				value++;
+			}
+
 			System.out.println("Finished");
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] value = { 0 };// We are using an integer array with one element to pass a single value as
-							// reference to another class. We cannot pass primitive values as reference in
-							// Java.
-		int incrementValue = 10000;
-		SimpleThreadSample sample = new SimpleThreadSample();
-		Increment firstOperation = sample.new Increment(value, incrementValue);
-		Increment secondOperation = sample.new Increment(value, incrementValue);
+		int incrementValue = 10_000;
+		Increment firstOperation = new Increment(incrementValue);
+		Increment secondOperation = new Increment(incrementValue);
 		firstOperation.start();
 		secondOperation.start();
+
 		try {
 			firstOperation.join();
 			secondOperation.join();
-			System.out.println("Value is " + value[0]);
+			System.out.println("Value is " + value);
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			Thread.currentThread().interrupt();
 		}
 	}
 }
