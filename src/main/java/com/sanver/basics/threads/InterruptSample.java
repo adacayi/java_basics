f3b17075804327a79c273467666847c4bd89e648
Abdullah Sanver <a_sanver@hotmail.com>
2024-08-08 16:20:46 +0100

    Refactor InterruptSample

--- src/main/java/com/sanver/basics/threads/InterruptSample.java
+++ src/main/java/com/sanver/basics/threads/InterruptSample.java
@@ -1,33 +1,29 @@
 package com.sanver.basics.threads;
 
+import static com.sanver.basics.utils.Utils.sleep;
+
 public class InterruptSample {
 
-	public static void main(String[] args) {
-		Thread waitingThread = new Thread(() -> {
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				System.out.println("Thread interrupted.");
-				System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());// If thread is
-																									// interrupted,
-																									// sleep will throw
-																									// an
-																									// InterruptedException
-																									// and clear the
-																									// interrupted flag.
-																									// Hence the value
-																									// is false.
-				Thread.currentThread().interrupt();
-				System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
-			}
-		});
-		waitingThread.start();
-		System.out.println("Thread started");
-		try {
-			Thread.sleep(2000);
-			waitingThread.interrupt();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        Thread waitingThread = new Thread(() -> {
+            try {
+                System.out.println("Thread started");
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+
+                // If thread is interrupted, sleep will throw an InterruptedException and clear the interrupted flag.
+                // Hence, the value is false.
+                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+
+                Thread.currentThread().interrupt();
+                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+            }
+        });
+
+        waitingThread.start();
+
+        sleep(2000);
+        waitingThread.interrupt();
+    }
 }
