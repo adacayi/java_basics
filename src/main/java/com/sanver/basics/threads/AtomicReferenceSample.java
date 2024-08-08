6250aab56dd44f9cadf7a9e85a887ac4ae0b4747
Abdullah Sanver <a_sanver@hotmail.com>
2024-08-08 15:48:08 +0100

    Add description to AtomicReferenceSample

--- src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
+++ src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
@@ -9,6 +9,15 @@ import java.util.stream.IntStream;
 import static com.sanver.basics.utils.LambdaExceptionUtil.rethrowConsumer;
 import static com.sanver.basics.utils.LambdaExceptionUtil.rethrowRunnable;
 
+/**
+ * The AtomicReference class in Java is part of the java.util.concurrent.atomic package and provides a way to work with object references atomically.
+ * This is useful in concurrent programming where multiple threads may need to update a shared reference without causing race conditions.
+ * <br><br>
+ * <b>Key Points:</b><br>
+ * <br><b>Atomic Operations:</b> Operations such as get, set, compareAndSet, and getAndSet on an AtomicReference are atomic. This means they are performed as a single, indivisible operation.
+ * <br><b>Thread Safety:</b> AtomicReference ensures that when one thread updates the reference, other threads see the updated value immediately without additional synchronization.
+ * <br><b>Use Cases:</b> It's commonly used in scenarios like non-blocking algorithms, where you need to ensure that a reference is updated only if it currently holds a specific value.
+ */
 public class AtomicReferenceSample {
 
     private static final int THREAD_COUNT = 10;
@@ -34,9 +43,9 @@ public class AtomicReferenceSample {
         for (int i = 0; i < 1000; i++) {
             boolean incremented = false;
 
-            while (!incremented) {
-                var existingValue = value.get();
-                var newValue = new IntValue(existingValue.getValue() + 1);
+            while (!incremented) { // If this while block did not exist, the final value will be less than 10,000.
+                IntValue existingValue = value.get();
+                IntValue newValue = new IntValue(existingValue.getValue() + 1);
                 // This will try to set the AtomicReference value to the new value, if the current value is the same object
                 // (not equals) i.e. existingValue object. If that is the case it will return true else false.
                 incremented = value.compareAndSet(existingValue, newValue);
