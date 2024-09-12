e352a9f8aa0cf5ca7a5558544bdc3f1fb049ac85
Abdullah Sanver <a_sanver@hotmail.com>
2024-09-12 17:43:10 +0100

    Add comments for ObjectMapper usage for deep copy and remove lombok, in DeepCopySample

--- src/main/java/com/sanver/basics/objects/DeepCopySample.java
+++ src/main/java/com/sanver/basics/objects/DeepCopySample.java
@@ -1,28 +1,46 @@
 package com.sanver.basics.objects;
 
 import com.google.gson.Gson;
-import lombok.Data;
-import lombok.NonNull;
+
+import java.util.StringJoiner;
 
 public class DeepCopySample {
     public static void main(String[] args) {
-        var person1 = new Person("Johnny", 16);
+        var person1 = new Person("Kimi", 18);
         var person2 = deepCopy(person1);
-        person1.setAge(1);
+        person1.name = "George";
+        person1.age = 26;
         System.out.println(person1);
         System.out.println(person2);
     }
 
     public static <T> T deepCopy(T object) {
-        // The advantage of using Gson instead of ObjectMapper is, it doesn't require a default constructor for the class to be deserialized
-        // https://www.baeldung.com/java-deep-copy
+        /* The advantages of using Gson instead of ObjectMapper are
+        1- It doesn't require a default constructor for the class to be deserialized
+        2- It doesn't require getters for fields for serialization (If there is no getter for a field, its value is not serialized. If no getter exists at all, objectMapper.writeValueAsString will throw an InvalidDefinitionException: No serializer found for class)
+        https://www.baeldung.com/java-deep-copy
+        */
         var gson = new Gson();
         return gson.<T>fromJson(gson.toJson(object), object.getClass());
     }
 
-    @Data
+
+
     static class Person {
-        @NonNull String name;
-        @NonNull int age;
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", Person.class.getSimpleName() + "[", "]")
+                    .add("name='" + name + "'")
+                    .add("age=" + age)
+                    .toString();
+        }
     }
 }
