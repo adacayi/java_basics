c6aab38a1bffa02e3dbc7c8ee68d0213d5f23fca
Abdullah Sanver <a_sanver@hotmail.com>
2024-09-17 17:08:37 +0100

    Add ThenApplyVsThenApplyAsync, add printForkJoinPool method to Utils

---
+++ src/main/java/com/sanver/basics/threads/completable_future/chaining/ThenApplyVsThenApplyAsync.java
@@ -0,0 +1,55 @@
+package com.sanver.basics.threads.completable_future.chaining;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntUnaryOperator;
+import java.util.function.UnaryOperator;
+
+import static com.sanver.basics.utils.Utils.printThreadPool;
+import static com.sanver.basics.utils.Utils.sleep;
+
+public class ThenApplyVsThenApplyAsync {
+    public static void main(String[] args) {
+        // thenApply uses the same executor as the previous CompletableFuture, whereas thenApplyAsync uses the ForkJoinPool.commonPool.
+        var executor = (ThreadPoolExecutor)Executors.newFixedThreadPool(2, new CustomThreadFactory("Custom-pool-worker"));
+
+        IntUnaryOperator square = x -> {
+            var thread = Thread.currentThread();
+            System.out.printf("Calculation of square of %3d  started. Thread id: %3d, Thread name: %s%n",x, thread.getId(), thread.getName());
+            sleep(2000);
+            System.out.printf("Calculation of square of %3d finished. Thread id: %3d, Thread name: %s%n",x, thread.getId(), thread.getName());
+            return x * x;
+        };
+
+        var first = CompletableFuture.supplyAsync(() -> square.applyAsInt(3), executor).thenApply(square::applyAsInt);
+        first.join();
+
+        var second = CompletableFuture.supplyAsync(() -> square.applyAsInt(7), executor).thenApplyAsync(square::applyAsInt);
+        second.join();
+
+        printThreadPool(executor, "State of the Custom pool after all operations finished");
+        // Even though the pool size is not zero (there are still threads (not active though)), since the threads are set as daemon, the main thread will exit without calling the executor.shutdown()
+    }
+
+    // Custom ThreadFactory to set thread name prefix
+    static class CustomThreadFactory implements ThreadFactory {
+        private final String namePrefix;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+        CustomThreadFactory(String namePrefix) {
+            this.namePrefix = namePrefix;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            // Customize the thread name with a prefix and unique number
+            String threadName = namePrefix + "-" + threadNumber.getAndIncrement();
+            Thread thread = new Thread(r, threadName);
+            thread.setDaemon(true);
+            return thread;
+        }
+    }
+}
--- src/main/java/com/sanver/basics/utils/Utils.java
+++ src/main/java/com/sanver/basics/utils/Utils.java
@@ -4,6 +4,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -30,6 +31,13 @@ public class Utils {
 
         var thread = Thread.currentThread();
         System.out.printf("Thread id: %-3d Thread name: %s%n%n", thread.getId(), thread.getName());
+    }
+
+    public static void printForkJoinPool(ForkJoinPool pool, String... info) {
+        displayInfo(info);
+
+        System.out.printf("Active threads in the fork join pool: %d%n", pool.getActiveThreadCount());
+        System.out.printf("The number of threads in the fork join pool: %d%n%n", pool.getPoolSize());
     }
 
     public static void printThreadPool(ThreadPoolTaskExecutor threadPool, String... info) {
