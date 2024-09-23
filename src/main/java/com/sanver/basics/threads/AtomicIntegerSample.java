1563a83cf46fffcdb2b90a274ecc7fa57f057ef4
Abdullah Sanver <a_sanver@hotmail.com>
2024-09-23 17:46:04 +0100

    Add accumulateAndGet usage to AtomicIntegerSample

--- src/main/java/com/sanver/basics/threads/AtomicIntegerSample.java
+++ src/main/java/com/sanver/basics/threads/AtomicIntegerSample.java
@@ -1,25 +1,23 @@
 package com.sanver.basics.threads;
 
+import java.text.NumberFormat;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class AtomicIntegerSample {
+    public static final AtomicInteger sum = new AtomicInteger(0); // Change this to int and increment sum by 3 inside the for loop to see the result might not be equal to 3,000,000.
 
     public static void main(String[] args) {
-        AtomicInteger value = new AtomicInteger(0);
-        Runnable increment = () -> {
-            for (int i = 0; i < 10000; i++, value.incrementAndGet())
-                ;
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100_000; i++) {
+                sum.incrementAndGet(); // Increment by 1
+                sum.accumulateAndGet(2, Integer::sum); // Increment by 2
+            }
         };
-        Thread first = new Thread(increment);
-        Thread second = new Thread(increment);
-        first.start();
-        second.start();
-        try {
-            first.join();
-            second.join();
-            System.out.println(value);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        var futures = IntStream.range(0, 10).mapToObj(i -> CompletableFuture.runAsync(runnable)).toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures).join();
+        System.out.println("Sum = " + NumberFormat.getInstance().format(sum));
     }
 }
