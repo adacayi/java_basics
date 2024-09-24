96924daa41db0afa4d19deb062574b863068453a
Abdullah Sanver <a_sanver@hotmail.com>
2024-09-24 16:00:43 +0100

    Simplify AtomicReferenceSample

--- src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
+++ src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
@@ -1,13 +1,9 @@
 package com.sanver.basics.threads;
 
 import java.text.NumberFormat;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.IntStream;
-
-import static com.sanver.basics.utils.RethrowAsUnchecked.uncheck;
 
 /**
  * The AtomicReference class in Java is part of the java.util.concurrent.atomic package and provides a way to work with object references atomically.
@@ -20,40 +16,28 @@ import static com.sanver.basics.utils.RethrowAsUnchecked.uncheck;
  */
 public class AtomicReferenceSample {
 
-    private static final int THREAD_COUNT = 10;
-    private static final AtomicReference<IntValue> ATOMIC_REFERENCE = new AtomicReference<>(new IntValue(0));
-    private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(THREAD_COUNT);
-
-    public static void main(String[] args) throws InterruptedException {
-        var executorService = Executors.newFixedThreadPool(THREAD_COUNT);
-        Callable<Void> increment = () -> {
-            uncheck(() -> increment(ATOMIC_REFERENCE));
-            return null;
-        };
-        var list = IntStream.range(0, 10).mapToObj(x -> increment).toList();
-        executorService.invokeAll(list);
-        System.out.println(NumberFormat.getInstance().format(ATOMIC_REFERENCE.get().getValue()));
-        executorService.shutdown();
+    public static void main(String[] args) {
+        var atomicReference = new AtomicReference<>(new IntValue(0));
+        var futures = IntStream.range(0, 10).mapToObj(x -> CompletableFuture.runAsync(() -> increment(atomicReference))).toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures).join();
+        System.out.println(NumberFormat.getInstance().format(atomicReference.get().getValue()));
     }
 
-    public static void increment(AtomicReference<IntValue> value) throws InterruptedException {
-        COUNT_DOWN_LATCH.countDown();
-        COUNT_DOWN_LATCH.await();
-
-        for (int i = 0; i < 1000; i++) {
+    public static void increment(AtomicReference<IntValue> value) {
+        for (int i = 0; i < 100_000; i++) {
             boolean incremented = false;
 
             while (!incremented) { // If this while block did not exist, the final value will be less than 10,000.
                 IntValue existingValue = value.get();
-                IntValue newValue = new IntValue(existingValue.getValue() + 1);
+                IntValue newValue = new IntValue(existingValue.getValue() + 3);
                 // This will try to set the AtomicReference value to the new value, if the current value is the same object
-                // (not equals) i.e. existingValue object. If that is the case it will return true else false.
+                // (not equals) i.e. existingValue object. If that is the case it will return true, else it will return false.
                 incremented = value.compareAndSet(existingValue, newValue);
             }
         }
     }
 
-    static class IntValue {
+    private static class IntValue {
         private final int value;
 
         public IntValue(int value) {
