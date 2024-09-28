1fdc02a2cd8a66b535841ed3d9064d79909f3bed
Abdullah Sanver <a_sanver@hotmail.com>
2024-09-28 15:21:59 +0100

    Add thread info to InterruptSample and InterruptWithExecutorSample.

--- src/main/java/com/sanver/basics/threads/InterruptSample.java
+++ src/main/java/com/sanver/basics/threads/InterruptSample.java
@@ -1,5 +1,6 @@
 package com.sanver.basics.threads;
 
+import static com.sanver.basics.utils.Utils.getThreadInfo;
 import static com.sanver.basics.utils.Utils.sleep;
 
 public class InterruptSample {
@@ -7,17 +8,17 @@ public class InterruptSample {
     public static void main(String[] args) {
         Thread waitingThread = new Thread(() -> {
             try {
-                System.out.println("Thread started");
+                System.out.println("Thread started. " + getThreadInfo());
                 Thread.sleep(10_000);
             } catch (InterruptedException e) {
-                System.out.println("Thread interrupted.");
+                System.out.println("Thread interrupted. " + getThreadInfo());
 
                 // If thread is interrupted, sleep will throw an InterruptedException and clear the interrupted flag.
                 // Hence, the value is false.
-                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+                System.out.printf("isInterruptedValue = %s. %s%n", Thread.currentThread().isInterrupted(), getThreadInfo());
 
                 Thread.currentThread().interrupt();
-                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+                System.out.printf("isInterruptedValue = %s.  %s%n ", Thread.currentThread().isInterrupted(), getThreadInfo());
             }
         });
 
--- src/main/java/com/sanver/basics/threads/InterruptWithExecutorThreads.java
+++ src/main/java/com/sanver/basics/threads/InterruptWithExecutorThreads.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import static com.sanver.basics.utils.Utils.getThreadInfo;
 import static com.sanver.basics.utils.Utils.sleep;
 
 public class InterruptWithExecutorThreads {
@@ -12,16 +13,15 @@ public class InterruptWithExecutorThreads {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         Future<?> future = executorService.submit(() -> {
             try {
+                System.out.println("Thread started. " + getThreadInfo());
                 Thread.sleep(10_000);
             } catch (InterruptedException e) {
-                System.out.println("Executor service thread interrupted.");
-                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+                System.out.println("Executor service thread interrupted. " + getThreadInfo());
+                System.out.printf("isInterruptedValue = %s. %s%n", Thread.currentThread().isInterrupted(), getThreadInfo());
                 Thread.currentThread().interrupt();
-                System.out.println("isInterruptedValue = " + Thread.currentThread().isInterrupted());
+                System.out.printf("isInterruptedValue = %s.  %s%n", Thread.currentThread().isInterrupted(), getThreadInfo());
             }
         });
-
-        System.out.println("Thread started");
 
         sleep(2000);
         future.cancel(true);
