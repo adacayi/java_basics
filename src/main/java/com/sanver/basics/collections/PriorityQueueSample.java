f9a22dc4599dd961d89ad42bd6173161b2f2eece
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-03 14:11:14 +0100

    Simplify PriorityQueueSample

--- src/main/java/com/sanver/basics/collections/PriorityQueueSample.java
+++ src/main/java/com/sanver/basics/collections/PriorityQueueSample.java
@@ -1,93 +1,38 @@
 package com.sanver.basics.collections;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
+/**
+ * PriorityQueue is an unbounded priority queue based on a priority heap. The elements of the priority queue are ordered according to their natural ordering, or by a Comparator provided at queue construction time, depending on which constructor is used. A priority queue does not permit null elements. A priority queue relying on natural ordering also does not permit insertion of non-comparable objects (Objects must implement Comparable) (doing so may result in ClassCastException).
+ * <br>Check <a href = https://www.geeksforgeeks.org/binary-heap>binary heap</a> for how it works.
+ */
 public class PriorityQueueSample {
-    static class Student {
-        private int id;
-        private String name;
-        private double cGPA;
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public double getcGPA() {
-            return cGPA;
-        }
-
-        public Student(int id, String name, double cGPA) {
-            this.id = id;
-            this.name = name;
-            this.cGPA = cGPA;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name, cGPA);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (!(obj instanceof Student))
-                return false;
-
-            Student second = (Student) obj;
-
-            if (second.cGPA == this.cGPA && second.id == this.id && second.name.equals(this.name))
-                return true;
-
-            return false;
-        }
-    }
-
-    static class Priorities {
-        private PriorityQueue<Student> priorityQueue;
-
-        public List<Student> getStudents(List<String> events) {
-            Comparator<Student> cGPAComparator = Comparator.comparing(Student::getcGPA).reversed().
-                    thenComparing(Student::getName).thenComparing(Student::getId);
-            priorityQueue = new PriorityQueue<>(cGPAComparator);
-            List<Student> result = new ArrayList<>();
-            Student student;
-
-            if (events == null || events.isEmpty())
-                return result;
-
-            for (String event : events) {
-                String[] splits = event.split(" ");
-
-                if (splits[0].equals("ENTER")) {
-                    student = new Student(Integer.parseInt(splits[3]), splits[1], Double.parseDouble(splits[2]));
-                    priorityQueue.add(student);
-                } else {
-                    priorityQueue.poll();//poll and remove are same but
-                    // remove throws exception when the queue is empty. poll does not.
-                }
-            }
-            while (!priorityQueue.isEmpty())
-                result.add(priorityQueue.remove());
-
-            return result;
-//        return Arrays.asList(priorityQueue.toArray(new Student[0]));// This won't return the queue in ordered list.
-        }
-    }
-
     public static void main(String... args) {
-        Priorities priorities = new Priorities();
-        String input = "SERVED\nSERVED\nENTER Shafaet 3.7 35\n" +
-                "ENTER Maria 3.6 46\n" +
-                "ENTER Dan 3.95 50";
-        List<String> events = Arrays.asList(input.split("\n"));
-        List<Student> expected = Arrays.asList(
+        List<Student> students = Arrays.asList(
+                new Student(46, "Maria", 3.6),
                 new Student(50, "Dan", 3.95),
-                new Student(35, "Shafaet", 3.7),
-                new Student(46, "Maria", 3.6));
-        List<Student> actual = priorities.getStudents(events);
-        System.out.printf("%s\n", Arrays.equals(expected.toArray(), actual.toArray()));
+                new Student(42, "Maria", 3.6),
+                new Student(35, "Shafaet", 3.7)
+        );
+        var studentComparator = Comparator.<Student, Double>comparing(x -> x.cGPA).thenComparing(x -> x.id);
+        var priorityQueue = new PriorityQueue<>(studentComparator); // If we did not specify a comparator, then Student class should be implementing Comparable to be able to use the NaturalComparator, otherwise we will get a ClassCastException.
+        // var priorityQueue = new PriorityQueue<>(); Try this instead to see the ClassCastException, since Student does not implement Comparable.
+
+        students.forEach(priorityQueue::offer); // Insert elements to the priority queue to form a min-heap. Priority queue uses an array to store the elements, with an initial capacity, which can be determined when constructed.
+
+        priorityQueue.forEach(System.out::println); // Notice that this does not need to be completely ordered based on the studentComparator, since this is a min-heap, a complete binary tree where each parent should be less than or equal to its children.
+        System.out.println();
+        System.out.println(String.join("\n", priorityQueue.toString().split("Student"))); // The order will be the same as the forEach
+        System.out.println();
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll()); // Retrieves and removes the head of this queue, which will always be the smallest element of the queue.
+            // poll and remove both retrieve and remove the head of the queue but remove throws an exception when the queue is empty, poll does not.
+        }
+    }
+
+    record Student(int id, String name, double cGPA) {
     }
 }
