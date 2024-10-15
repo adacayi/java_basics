96a0434f6f2287741ffa00e7ccd1b551d2396db5
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-15 14:56:39 +0100

    Add explanation for BitSet size and the methods "or" and "and".

--- src/main/java/com/sanver/basics/collections/BitSetSample.java
+++ src/main/java/com/sanver/basics/collections/BitSetSample.java
@@ -5,8 +5,10 @@ import java.util.BitSet;
 public class BitSetSample {
 
     public static void main(String[] args) {
-        int i, length = 10;
-        BitSet divisibleBy2 = new BitSet(length);
+        int i;
+        int length = 10;
+        BitSet divisibleBy2 = new BitSet(length); // BitSets are packed into arrays of "words." Currently, a word is a long, which consists of 64 bits. BitSet(length) will initialize a long array of size (length - 1) >> 6 + 1 since every long can hold 64 bits.
+        System.out.println("divisibleBy2 size (number of bits it can hold): " + divisibleBy2.size()); // Size of the long array times 64 since every long can hold 64 bits.
         BitSet divisibleBy3 = new BitSet(length);
         BitSet divisibleBy2And3;
         BitSet divisibleBy2Or3;
@@ -20,12 +22,12 @@ public class BitSetSample {
 
         divisibleBy2Or3 = (BitSet) divisibleBy2.clone();
         divisibleBy2And3 = (BitSet) divisibleBy2.clone();
-        divisibleBy2Or3.or(divisibleBy3);
-        divisibleBy2And3.and(divisibleBy3);
-        System.out.printf("Divisible by 2: %s\n", divisibleBy2);
-        System.out.printf("Divisible by 3: %s\n", divisibleBy3);
-        System.out.printf("Divisible by 2 or 3: %s\n", divisibleBy2Or3);
-        System.out.printf("Divisible by 2 and 3: %s\n", divisibleBy2And3);
+        divisibleBy2Or3.or(divisibleBy3); // The size of the BitSet will increase to the BitSet in the "or" method's argument if the argument's size is larger, otherwise it won't change.
+        divisibleBy2And3.and(divisibleBy3); // The size of the BitSet will decrease to the BitSet in the "and" method's argument if the argument's size is smaller, otherwise it won't change.
+        System.out.printf("Divisible by 2: %s%n", divisibleBy2);
+        System.out.printf("Divisible by 3: %s%n", divisibleBy3);
+        System.out.printf("Divisible by 2 or 3: %s%n", divisibleBy2Or3);
+        System.out.printf("Divisible by 2 and 3: %s%n", divisibleBy2And3);
 
         System.out.print("Divisible by 2 and 3 content: ");
         for (i = 0; i < divisibleBy2And3.length(); i++) // length returns the index of the last true bit plus 1.
@@ -33,8 +35,9 @@ public class BitSetSample {
             System.out.printf("%s, ",divisibleBy2And3.get(i));
 
         System.out.println();
-        int size = divisibleBy2And3.size(); // Size is larger than the last bit with number 1
+        int size = divisibleBy2And3.size();
         System.out.println("Size of divisible by 2 and 3 BitSet is " + size);
+        System.out.println("length() for divisible by 2 and 3 BitSet is (length returns the index of the last true bit plus 1) " + divisibleBy2And3.length());
 
         for (i = 0; i < size; i++) {
             if (divisibleBy2And3.get(i))
