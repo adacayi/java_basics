20df68b1a1a0c19068f66936b9ae1f6b2251f82c
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-21 15:00:54 +0100

    Add another way to count bits in BitCount

--- src/main/java/com/sanver/basics/basicoperators/BitCount.java
+++ src/main/java/com/sanver/basics/basicoperators/BitCount.java
@@ -6,5 +6,7 @@ public class BitCount {
         System.out.println("Bit count for 31    : " + Integer.bitCount(31));
         System.out.println("Bit count for -1    : " + Integer.bitCount(-1));
         System.out.println("Bit count for -1L   : " + Long.bitCount(-1L));
+        System.out.println("Bit count for (1L << 33) + 7   : " + Long.toString((1L << 33) + 7, 2).chars().filter(x -> x == '1').count()); // Another way to count bits. This only works for non-negative numbers, since Long.toString returns the mathematical representation of the number in radix and puts a minus sign if it is negative.
+        System.out.println("(1L << 33) + 7 : " + Long.toString((1L << 33) + 7, 2));
     }
 }
