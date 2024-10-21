f760a92b843103871356a15e0063992e7289afe6
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-21 14:46:28 +0100

    Add BitCount

---
+++ src/main/java/com/sanver/basics/math/BitCount.java
@@ -0,0 +1,10 @@
+package com.sanver.basics.math;
+
+public class BitCount {
+    public static void main(String[] args) {
+        System.out.println("Bit count for 1 << 5: "+ Integer.bitCount(1 << 5));
+        System.out.println("Bit count for 31    : "+ Integer.bitCount(31));
+        System.out.println("Bit count for -1    : "+ Integer.bitCount(-1));
+        System.out.println("Bit count for -1L   : "+ Long.bitCount(-1L));
+    }
+}
