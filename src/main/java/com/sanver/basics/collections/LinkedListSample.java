75d8f2a2c304521999744763aac48607c4e7edfb
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-23 15:40:37 +0100

    Add addFirst, getFirst and getLast usages to LinkedListSample

--- src/main/java/com/sanver/basics/collections/LinkedListSample.java
+++ src/main/java/com/sanver/basics/collections/LinkedListSample.java
@@ -1,27 +1,33 @@
 package com.sanver.basics.collections;
 
-import java.util.ListIterator;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.ListIterator;
 
 public class LinkedListSample {
 
-	public static void main(String[] args) {
-		List<String> list = new LinkedList<>(Arrays.asList("Ahmet","Mustafa","Muhammed"));
-		list.add("Ibrahim");
-		list.forEach(System.out::println);
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("Ahmet", "Mustafa", "Muhammed"));
+        list.addFirst("Ibrahim");
+        list.forEach(System.out::println);
 
-		System.out.println();
+        System.out.println();
 
-		ListIterator<String> iterator = list.listIterator(list.size());
+        ListIterator<String> iterator = list.listIterator(list.size());
 
-		while (iterator.hasPrevious())
-			System.out.println(iterator.previous());
+        while (iterator.hasPrevious()) {
+            System.out.println(iterator.previous());
+        }
 
-		System.out.println();
+        System.out.println();
 
-		for (int i = 0; i < list.size(); i++)
-			System.out.println(list.get(i));
-	}
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        System.out.println();
+
+        System.out.println("list.getFirst(): " + list.getFirst());
+        System.out.println("list.getLast() : " + list.getLast());
+    }
 }
