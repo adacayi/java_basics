49bc5db504202b7583d9e9c23da5eb109e642322
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-23 12:27:25 +0100

    Improve the descriptions in HashSetSortingWithItsHashCode

--- src/main/java/com/sanver/basics/collections/HashSetSortingWithItsHashCode.java
+++ src/main/java/com/sanver/basics/collections/HashSetSortingWithItsHashCode.java
@@ -1,14 +1,12 @@
 package com.sanver.basics.collections;
 
-import lombok.Value;
-
 import java.util.HashSet;
 import java.util.Set;
 
 public class HashSetSortingWithItsHashCode {
 
     // This is the method where the eventual hash code is calculated for the HashMap.
-    // The final index of the element is calculated by hash & (capacity -1), where capacity is the length of the backed array of the HashMap
+    // The final index of the element is calculated by hash & (capacity -1), where capacity is the length of the backed array of the HashMap, which is always a power of 2.
     // That formula ensures the final index values are smaller than the length of the array.
     static int hash(Object key) {
         int h;
@@ -21,17 +19,14 @@ public class HashSetSortingWithItsHashCode {
         set.add(new Person("Ahmet"));
         set.add(new Person("Mustafa"));
         System.out.println(set);
-        set.forEach(t -> System.out.printf("Value: %-12s Object hash code: %-12s Hash code for the map(object hash code ^ (object hash code >>> 16)): %-12s Index for the map which has a backed array of length 16: %s\n", t, t.hashCode(), hash(t), hash(t) & (16 - 1)));
+        set.forEach(t -> System.out.printf("Value: %-17s Object hash code: %,14d  Hash code for the map (object hash code ^ (object hash code >>> 16)): %,14d  Index for the map which has a backed array of length 16: %,2d%n", t, t.hashCode(), hash(t), hash(t) & (16 - 1)));
         System.out.println("The implementation of the forEach goes over each element of the backed array of the map for the set (HashSet is actually using a HashMap to store its values), so the order depends on the calculated index for that array");
     }
 
-    @Value
-    static class Person {
-        String name;
-
+    record Person(String name) {
         @Override
         public String toString() {
-            return name;
+            return "Person(" + name + ")";
         }
     }
 }
