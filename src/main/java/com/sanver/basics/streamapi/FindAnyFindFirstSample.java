49d1ad76863645736998f2ccba7c66b03ad82f92
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-29 19:06:33 +0000

    Improve comments in FindAnyFindFirstSample

--- src/main/java/com/sanver/basics/streamapi/FindAnyFindFirstSample.java
+++ src/main/java/com/sanver/basics/streamapi/FindAnyFindFirstSample.java
@@ -10,12 +10,7 @@ public class FindAnyFindFirstSample {
 		Supplier<IntStream> getStream = () -> IntStream.of(3, 2, 1, 5);
 		System.out
 				.println("Stream is " + getStream.get().mapToObj(Integer::toString).collect(Collectors.joining(", ")));
-		System.out.println("findFirst result: " + getStream.get().parallel().findFirst().orElse(-1));// We used parallel
-																										// stream, else
-																										// the results
-																										// would be
-																										// same.
-		System.out.println("findAny result: " + getStream.get().parallel().findAny().orElse(-1));
+		System.out.println("findFirst result: " + getStream.get().parallel().findFirst().orElse(-1)); // If the stream has order, findFirst always returns the first element if the stream is not empty.
+		System.out.println("findAny result: " + getStream.get().parallel().findAny().orElse(-1)); // findAny returns an OptionalInt describing some element of the stream, or an empty OptionalInt if the stream is empty. The behavior of this operation is explicitly nondeterministic; it is free to select any element in the stream. This is to allow for maximal performance in parallel operations
 	}
-
 }
