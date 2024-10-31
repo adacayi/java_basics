6a8e07257073b6c0bbd2edc5954982183583d451
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-31 12:58:33 +0000

    Add sonar warning for using peek in streams to PeekSample

--- src/main/java/com/sanver/basics/streamapi/PeekSample.java
+++ src/main/java/com/sanver/basics/streamapi/PeekSample.java
@@ -1,15 +1,19 @@
 package com.sanver.basics.streamapi;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Sonar issue for peek:
+ * <p>
+ * According to its JavaDocs, the intermediate Stream operation java.util.Stream.peek() “exists mainly to support debugging” purposes.
+ * A key difference with other intermediate Stream operations is that the Stream implementation is free to skip calls to peek() for optimization purpose. This can lead to peek() being unexpectedly called only for some or none of the elements in the Stream.
+ * As a consequence, relying on peek() without careful consideration can lead to error-prone code.
+ */
 public class PeekSample {
 
     public static void main(String[] args) {
         int personCount = 10;
-        List<Integer> idList = IntStream.range(0, personCount).boxed().collect(Collectors.toList());
-        idList.parallelStream().peek(x->System.out.printf("%d ",x)).collect(Collectors.toList());
-        // Collect is called because streams have lazy execution.
+        IntStream.range(0, personCount).parallel().peek(x -> System.out.printf("%d ", x)).findFirst();
+        // findFirst is called because streams have lazy execution.
     }
 }
