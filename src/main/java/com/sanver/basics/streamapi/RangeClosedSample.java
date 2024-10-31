16f53f6aa1a753a213e00bdf59bfd843f0582664
Abdullah Sanver <a_sanver@hotmail.com>
2024-10-31 13:00:09 +0000

    Simplify RangeClosedSample

--- src/main/java/com/sanver/basics/streamapi/RangeClosedSample.java
+++ src/main/java/com/sanver/basics/streamapi/RangeClosedSample.java
@@ -1,31 +1,9 @@
 package com.sanver.basics.streamapi;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RangeClosedSample {
-
-	class Person {
-		int id;
-		String name;
-
-		public String toString() {
-			return id + " " + name;
-		}
-	}
-
-	public static void main(String[] args) {
-		int personCount = 16;
-		RangeClosedSample sample = new RangeClosedSample();
-
-		List<Person> people = IntStream.rangeClosed(1, personCount).mapToObj(x -> {
-			Person p = sample.new Person();
-			p.id = x;
-			p.name = "Abdullah_" + x;
-			return p;
-		}).collect(Collectors.toList());
-
-		people.forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 10).forEach(System.out::println);
+    }
 }
