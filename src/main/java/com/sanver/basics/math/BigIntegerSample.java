04119fb19925089104885b9540eb28c3a89bd42a
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-01 17:28:15 +0000

    Refactor BigIntegerSample

--- src/main/java/com/sanver/basics/math/BigIntegerSample.java
+++ src/main/java/com/sanver/basics/math/BigIntegerSample.java
@@ -2,16 +2,35 @@ package com.sanver.basics.math;
 
 import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BigIntegerSample {
     public static void main(String... args) {
-        BigInteger a, b;
+        BigInteger first;
+        BigInteger second;
+
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter first integer: ");
-            a = new BigInteger(scanner.nextLine());
+            System.out.print("Enter first  integer: ");
+            first = new BigInteger(scanner.nextLine());
             System.out.print("Enter second integer: ");
-            b = new BigInteger(scanner.nextLine());
-            System.out.printf("First: %s\nSecond: %s\nAddition: %s\nMultiplication: %s\n", a, b, a.add(b), a.multiply(b));
+            second = new BigInteger(scanner.nextLine());
+
+            print(first, second, BigInteger::add, "+");
+            print(first, second, BigInteger::multiply, "x");
         }
+    }
+
+    private static void print(BigInteger first, BigInteger second, BinaryOperator<BigInteger> operator, String operatorSign) {
+        var result = operator.apply(first, second);
+        var max = result.toString().length();
+        var format = "%" + max + "s%n";
+        System.out.println();
+        System.out.printf(format, first);
+        System.out.printf(format, second);
+        System.out.println(operatorSign);
+        System.out.printf("%s%n", IntStream.range(0, max).mapToObj(x -> "-").collect(Collectors.joining("")));
+        System.out.println(result);
     }
 }
