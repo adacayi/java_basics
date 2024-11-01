73618c41c3b9651e5a0eb505c783b3a687ceb56c
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-01 18:18:49 +0000

    Refactor PossiblePrimeSample

--- src/main/java/com/sanver/basics/math/PossiblePrimeSample.java
+++ src/main/java/com/sanver/basics/math/PossiblePrimeSample.java
@@ -1,6 +1,9 @@
 package com.sanver.basics.math;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class PossiblePrimeSample {
@@ -8,7 +11,10 @@ public class PossiblePrimeSample {
         System.out.print("Enter a number to check if it is possibly prime: ");
         try (Scanner scanner = new Scanner(System.in)) {
             BigInteger number = new BigInteger(scanner.next());
-            System.out.printf("%s\n", number.isProbablePrime(1) ? "prime" : "not prime");
+            var certainty = 4;
+            var possibility = BigDecimal.valueOf(1).subtract(new BigDecimal("0.5").pow(certainty)).multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP);
+            var format = NumberFormat.getInstance();
+            System.out.printf("%s: %s%n", format.format(number), number.isProbablePrime(4) ? "More than %" + possibility + " probable that it is prime" : "definitely not prime"); // Certainty parameter: A measure of the uncertainty that the caller is willing to tolerate: if the call returns true the probability that this BigInteger is prime exceeds (1 - (1/2)^certainty). The execution time of this method is proportional to the value of this parameter.
         }
     }
 }
