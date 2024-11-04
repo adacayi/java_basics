2337831f06917c0badb5f85b8a9eaaa5e0afc78d
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-04 17:25:03 +0000

    Refactor comment for the need for paranthesis in IPMatching

--- src/main/java/com/sanver/basics/regex/IPMatching.java
+++ src/main/java/com/sanver/basics/regex/IPMatching.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class IPMatching {
     public static void main(String... args) {
         String zeroTo255 = "(\\d{1,2}|[01]\\d{2}|2[0-4]\\d|25[0-5])";
-        //Paranthesis are required in the beginning and at the end because we will join this 4 times with dots.
+        //Paranthesis are required in the beginning and at the end because we have | and without paranthesis it will evaluate all the expression until the next | and since we join these zeroTo255 4 times, we need the separation.
         String pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
