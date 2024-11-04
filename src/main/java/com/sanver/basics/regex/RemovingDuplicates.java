b6ee221b8b18a2abcab47161b466e5af11dc8eaf
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-04 21:03:08 +0000

    Fix issue in the repetition group which allows removing strings containing duplicate word, refactor output.

--- src/main/java/com/sanver/basics/regex/RemovingDuplicates.java
+++ src/main/java/com/sanver/basics/regex/RemovingDuplicates.java
@@ -5,24 +5,27 @@ import java.util.regex.Pattern;
 
 public class RemovingDuplicates {
     public static void main(String... args) {
-        boolean isReplaced;
-        //This application is to remove any duplicate words.
-        String message = "Goodbye bye Bye in iN in the end bye good";
-        String regex = "(\\b\\w+\\b)(.*?)(\\1\\b\\s*)+";// \b is used to mean beginning or end of a word.
+        //This application removes any duplicate words case insensitively with the accompanying spaces.
+        String message = "Goodbye bye byeBye in iN in the end Bye byE to you, bYe good bye";
+        String regex = "(\\b\\w+\\b)(.+?)((\\b\\1\\b\\s?)+)";// \b is used to mean beginning or end of a word.
         // \1 is used to show the first captured group
+        // \\s* is used to capture the spaces after the duplicate words, so they can be removed as well.
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        System.out.println(message);
+        Matcher matcher;
+        boolean duplicateExists;
+        System.out.printf("Message %8s: %s%n", "",message);
 
         do {
-            Matcher matcher = pattern.matcher(message);
-            isReplaced = false;
-            System.out.println("\n---\n");
-            while (matcher.find()) {
-                System.out.println(matcher.group());
-                message = message.replaceAll(matcher.group(), matcher.group(1) + matcher.group(2));
-                System.out.println(message);
-                isReplaced = true;
+            matcher = pattern.matcher(message);
+            System.out.println("\n");
+            duplicateExists = matcher.find();
+            if (duplicateExists) {
+                System.out.println("Repetition group: " + matcher.group());
+                System.out.println("Repeating word  : " + matcher.group(1));
+                System.out.println("Repetition      : " + matcher.group(3));
+                message = message.replace(matcher.group(), matcher.group(1) + matcher.group(2));
+                System.out.printf("After removal   : %s%n", message);
             }
-        } while (isReplaced);
+        } while (duplicateExists);
     }
 }
