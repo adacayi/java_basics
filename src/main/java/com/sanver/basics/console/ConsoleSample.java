adfaed973acbc0d9fd2d1662c33c0b6a9a875545
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-05 15:13:38 +0000

    Add comments for how to run ConsoleSample from the terminal

--- src/main/java/com/sanver/basics/console/ConsoleSample.java
+++ src/main/java/com/sanver/basics/console/ConsoleSample.java
@@ -5,7 +5,7 @@ import java.io.Console;
 public class ConsoleSample {
 
 	public static void main(String[] args) {
-		Console console = System.console();// console will be null if you run it from Eclipse. Run it from command line
+		Console console = System.console();// System.console() will be null if you run it from IntelliJ or Eclipse. Run it from command line. Go to Basics\target\classes in the terminal and type java com.sanver.basics.console.ConsoleSample
 		String userName = console.readLine("User Name: ");
 		char[] password = console.readPassword("Password: ");
 		System.out.println("Selamunaleykum " + userName);
