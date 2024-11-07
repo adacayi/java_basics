8af0601d0268fe53480bd3ed9cee033ea3e23663
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-07 15:44:04 +0000

    Add orElseThrow usage to OptionalSample

--- src/main/java/com/sanver/basics/streamapi/OptionalSample.java
+++ src/main/java/com/sanver/basics/streamapi/OptionalSample.java
@@ -1,6 +1,7 @@
 package com.sanver.basics.streamapi;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
@@ -9,6 +10,7 @@ public class OptionalSample {
 
     public static void main(String[] args) {
         var names = List.of("abc", "abd", "bcd");
+        System.out.println("names list: " + names);
         Optional<String> optionalA = names
                 .stream()
                 .filter(x -> x.startsWith("a"))
@@ -17,9 +19,20 @@ public class OptionalSample {
                 .stream()
                 .filter(x -> x.startsWith("c"))
                 .findFirst();
-        System.out.println(optionalA.map(x -> "name starting with a " + x).orElse("na"));
-        System.out.println(optionalC.map(x -> "name starting with c " + x).orElse("na"));
-        System.out.println("optionalA: " + optionalA); // notice map does not change optionalA. Since Optional class is immutable in java.
+        System.out.println(optionalA.map(x -> "First name starting with a: " + x).orElse("No names starting with a."));
+        System.out.println(optionalC.map(x -> "First name starting with c: " + x).orElse("No names starting with c."));
+        try {
+            System.out.println("First name starting with c:" + optionalC.orElseThrow());
+        } catch (NoSuchElementException e) {
+            System.out.println("No names starting with c. Thrown exception: " + e);
+        }
+
+        try {
+            System.out.println("First name starting with c:" + optionalC.orElseThrow(() -> new IllegalArgumentException("Start letter not found")));
+        } catch (IllegalArgumentException e) {
+            System.out.println("No names starting with c. Thrown exception: " + e);
+        }
+        System.out.println("\noptionalA: " + optionalA); // notice map does not change optionalA. Since Optional class is immutable in java.
         System.out.println("optionalC: " + optionalC);
         String value = null;
         // Optional<String> optional=Optional.of(value);// This will give a runtime
