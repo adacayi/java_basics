cf0a5b135bb341ec0c3697b76542e8ee178f7afb
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-08 16:14:00 +0000

    Refactor StringBufferSynchronizationTest

--- src/main/java/com/sanver/basics/strings/StringBufferSynchronizationTest.java
+++ src/main/java/com/sanver/basics/strings/StringBufferSynchronizationTest.java
@@ -1,38 +1,35 @@
 package com.sanver.basics.strings;
 
+import java.util.concurrent.CompletableFuture;
+
+import static com.sanver.basics.utils.Utils.sleep;
+
 public class StringBufferSynchronizationTest {
 
-	public static void main(String[] args) {
-		StringBuffer buffer = new StringBuffer();
-		int count = 100000;
-		
-		Runnable append = () -> {
-			for (int i = 0; i < count; i++)
-				buffer.append("a");
-		};
-		Runnable remove = () -> {
-			for (int i = 0; i < count;) {
-				if (buffer.length() > 0) {
-					buffer.deleteCharAt(0);
-					i++;
-				}
-			}
-		};
+    public static void main(String[] args) {
+        var buffer = new StringBuffer();
+        int count = 100000;
 
-		Thread t1 = new Thread(append);
-		Thread t2 = new Thread(remove);
-		t1.start();
-		t2.start();
-		
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("Finished. Buffer size= " + buffer.length());// Since StringBuffer is synchronised the size
-																		// is 0. If we used StringBuilder instead the
-																		// code would not end always.
-	}
+        Runnable append = () -> {
+            for (int i = 0; i < count; i++)
+                buffer.append("a");
+        };
+
+        Runnable delete = () -> {
+            for (int i = 0; i < count; i++) {
+                while (buffer.isEmpty()) {
+                    sleep(1);
+                }
+                buffer.deleteCharAt(0);
+            }
+        };
+
+        var appendFuture = CompletableFuture.runAsync(append);
+        var deleteFuture = CompletableFuture.runAsync(delete);
+        CompletableFuture.allOf(appendFuture, deleteFuture).join();
+
+        System.out.println("Finished. Buffer length = " + buffer.length());// Since StringBuffer is synchronised the size
+        // is 0. If we used StringBuilder instead the
+        // code would not end always.
+    }
 }
