7fc936c01d7456e35a300ac958301f800d03f102
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-08 12:05:40 +0000

    Add brackets to the splitted items to show there are no spaces trailing them

--- src/main/java/com/sanver/basics/strings/SplitSample.java
+++ src/main/java/com/sanver/basics/strings/SplitSample.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class SplitSample {
     public static void main(String... args) {
-        String value = "Selamunaleykum. This will    be_separated, won't it?";
-        String[] split = value.split("[^A-z'?,.]+");// This regex means excluding characters from A to z
+        String value = "Selamunaleykum. This#will    be_separated, won't it?";
+        String[] split = value.split("[^A-Za-z'?,.]+");// This regex means excluding characters from A to z
         // (The characters in ASCII table between A and z which includes underscore.) and also excluding '?,.
         // So, we will see '?,. on the splits and "be_separated" won't be separated.
-        // The separation is caused by the spaces in the above case.
+        // The separation is caused by the spaces and # in the above scenario.
         // We used + so that consecutive separator characters will be considered as one separator,
         // hence there won't be empty strings in the split array.
-        Arrays.asList(split).forEach(System.out::println);
+        Arrays.asList(split).forEach(x -> System.out.printf("[%s]%n", x));
         //Same cannot be achieved by StringTokenizer, since it does not take Regex.
     }
 }
