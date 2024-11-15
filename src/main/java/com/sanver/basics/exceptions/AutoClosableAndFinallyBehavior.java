2c594a7b8d5bdd39999099c32d78d8c8b5bcb3c7
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-15 16:43:35 +0000

    Add AutoClosableAndFinallyBehavior

---
+++ src/main/java/com/sanver/basics/exceptions/AutoClosableAndFinallyBehavior.java
@@ -0,0 +1,33 @@
+package com.sanver.basics.exceptions;
+
+public class AutoClosableAndFinallyBehavior {
+    public static void main(String[] args) {
+        try (var obj = new MyClosableClass(1);
+             var obj2 = new MyClosableClass(2)) { // last object is closed first.
+            obj.myMethod();
+        } catch (Exception e) {
+            System.out.println(3);
+        } finally {
+            System.out.println(4);
+        }
+
+    }
+
+    static class MyClosableClass implements AutoCloseable {
+        final int id;
+
+        public MyClosableClass(final int id) {
+            this.id = id;
+        }
+
+        void myMethod() {
+            System.out.println(5);
+            throw new RuntimeException("Some exception");
+        }
+
+        @Override
+        public void close() {
+            System.out.println(id);
+        }
+    }
+}
