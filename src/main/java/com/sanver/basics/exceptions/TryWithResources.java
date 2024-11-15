9c9a5fff1280ad0405f319630044ce55fe37edb1
Abdullah Sanver <a_sanver@hotmail.com>
2024-11-15 16:29:02 +0000

    Add comments to TryWithResources

--- src/main/java/com/sanver/basics/exceptions/TryWithResources.java
+++ src/main/java/com/sanver/basics/exceptions/TryWithResources.java
@@ -9,10 +9,10 @@ public class TryWithResources {
 	public static void main(String[] args) {
 		try (FileInputStream stream = new FileInputStream("trial.txt")) {
 			stream.read();
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		} catch (IOException e1) {
-			System.out.println(e1.getMessage());
+		} catch (FileNotFoundException e) { // If we had this after the catch(IOException) it would result in a "FileNotFoundException has already been caught" error, because catch(IOException) would have caught it.
+			System.out.println("FileNotFoundException: " + e.getMessage());
+		} catch (IOException e1) { // This block will not be executed since stream.read() throws a FileNotFoundException in this case, and it is caught in the first catch block.
+			System.out.println("IOException: " + e1.getMessage());
 		}
 	}
 }
