fd9d810e2a5b03619a27be86b99f700ab8a986e6
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-05 14:51:04 +0000

    Refactor ObjectEqualityBasedOnFields to use instanceOf pattern matching and use instanceof for type check.

--- src/main/java/com/sanver/basics/objects/ObjectEqualityBasedOnFields.java
+++ src/main/java/com/sanver/basics/objects/ObjectEqualityBasedOnFields.java
@@ -4,7 +4,18 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * The main purpose of this class is to show how <code>Objects.equals()</code> and <code>Objects.deepEquals()</code> work
+ */
 public class ObjectEqualityBasedOnFields {
+    public static void main(String... args) {
+        Student first = new Student("Ahmet", 23, new String[]{"Maths", "Science", "Sports"});
+        Student second = new Student("Ahmet", 23, new String[]{"Maths", "Science", "Sports"});
+        System.out.printf("%s%n", first.equals(second));
+        System.out.printf("%d %d %nFirst hashcode equals second = %s%n", first.hashCode(), second.hashCode(),
+                first.hashCode() == second.hashCode());
+    }
+
     // Instead of overriding hashcode and equals methods as shown below so that if fields are equal, equals return true and hashcode is the same,
     // we can use lombok @EqualsAndHashCode annotation. Note the hashcode with lombok value will be different from the below implementation.
     // Check the .class file for the lombok generated code.
@@ -13,7 +24,7 @@ public class ObjectEqualityBasedOnFields {
         private int age;
         private String[] lessons;
 
-        public Student(String name, int age, String[] lessons) {
+        public Student(final String name, final int age, final String[] lessons) {
             this.name = name;
             this.age = age;
             this.lessons = lessons;
@@ -22,7 +33,8 @@ public class ObjectEqualityBasedOnFields {
         @Override
         public int hashCode() {
             Field[] fields = this.getClass().getDeclaredFields();
-            int i, n = fields.length;
+            int i;
+            int n = fields.length;
             Object[] values = new Object[n];
             Object obj;
             for (i = 0; i < n; i++) {
@@ -41,20 +53,28 @@ public class ObjectEqualityBasedOnFields {
 
         @Override
         public boolean equals(Object obj) {
-            if (!this.getClass().isInstance(obj))
+            if (obj == this) {
+                return true;
+            }
+
+            if (!(obj instanceof Student)) {
                 return false;
+            }
 
             Field[] fields = this.getClass().getDeclaredFields();
-            Object first, second;
+            Object first;
+            Object second;
             for (Field field : fields) {
                 try {
                     first = field.get(this);
                     second = field.get(obj);
                     if (field.getType().isArray()) {
-                        if (!Objects.deepEquals(first, second))
+                        if (!Objects.deepEquals(first, second)) {
                             return false;
-                    } else if (!Objects.equals(first, second))
+                        }
+                    } else if (!Objects.equals(first, second)) {
                         return false;
+                    }
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
@@ -64,30 +84,22 @@ public class ObjectEqualityBasedOnFields {
     }
 
     public static int arrayHashCode(Object obj) {
-        if (obj instanceof Object[])
-            return Arrays.deepHashCode((Object[]) obj);
-        if (obj instanceof byte[])
-            return Arrays.hashCode((byte[]) obj);
-        if (obj instanceof short[])
-            return Arrays.hashCode((short[]) obj);
-        if (obj instanceof int[])
-            return Arrays.hashCode((int[]) obj);
-        if (obj instanceof long[])
-            return Arrays.hashCode((long[]) obj);
-        if (obj instanceof char[])
-            return Arrays.hashCode((char[]) obj);
-        if (obj instanceof float[])
-            return Arrays.hashCode((float[]) obj);
-        if (obj instanceof double[])
-            return Arrays.hashCode((double[]) obj);
+        if (obj instanceof Object[] objectArray)
+            return Arrays.deepHashCode(objectArray);
+        if (obj instanceof byte[] byteArray)
+            return Arrays.hashCode(byteArray);
+        if (obj instanceof short[] shortArray)
+            return Arrays.hashCode(shortArray);
+        if (obj instanceof int[] intArray)
+            return Arrays.hashCode(intArray);
+        if (obj instanceof long[] longArray)
+            return Arrays.hashCode(longArray);
+        if (obj instanceof char[] charArray)
+            return Arrays.hashCode(charArray);
+        if (obj instanceof float[] floatArray)
+            return Arrays.hashCode(floatArray);
+        if (obj instanceof double[] doubleArray)
+            return Arrays.hashCode(doubleArray);
         return Arrays.hashCode((boolean[]) obj);
-    }
-
-    public static void main(String... args) {
-        Student first = new Student("Ahmet", 23, new String[]{"Maths", "Science", "Sports"});
-        Student second = new Student("Ahmet", 23, new String[]{"Maths", "Science", "Sports"});
-        System.out.printf("%s\n", first.equals(second));
-        System.out.printf("%d %d \nFirst hashcode equals second = %s\n", first.hashCode(), second.hashCode(),
-                first.hashCode() == second.hashCode());
     }
 }
