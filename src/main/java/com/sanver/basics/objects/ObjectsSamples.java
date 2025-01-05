9b99bc2b780233a3b0c8a04e19296fe0ddd6e68b
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-05 14:55:38 +0000

    Refactor ObjectsSample to replace \n with %n and add description for the outputs

--- src/main/java/com/sanver/basics/objects/ObjectsSamples.java
+++ src/main/java/com/sanver/basics/objects/ObjectsSamples.java
@@ -9,15 +9,24 @@ public class ObjectsSamples {
         Object b = new Object();
         Object c = null;
         Object d = b;
-        System.out.printf("%d\n", Objects.hashCode(a));
-        System.out.printf("%d\n", Objects.hashCode(b));
-        System.out.printf("%s\n", Objects.equals(a, b));
-        System.out.printf("%s\n", Objects.equals(a, c));
-        System.out.printf("%s\n", Objects.equals(b, d));
-        String[] first = {"Ahmet", "Mustafa", "Muhammed"}, second = {"Ahmet", "Mustafa", "Muhammed"};
-        System.out.printf("%s\n", Objects.equals(first, second));
-        System.out.printf("%s\n", Objects.deepEquals(first, second));
-        System.out.printf("%s %s\n", Objects.hashCode(first), Objects.hashCode(second));
-        System.out.printf("%s %s\n", Arrays.hashCode(first), Arrays.hashCode(second));
+        String[] first = {"Ahmet", "Mustafa", "Muhammed"};
+        String[] second = {"Ahmet", "Mustafa", "Muhammed"};
+
+        var format1 = "%-20s: %d%n";
+        var format2 = "%-20s: %s%n";
+        var format3 = "%-33s: %s%n";
+        var format4 = "%-49s: %s %s%n";
+
+        System.out.printf(format1, "Objects.hashCode(a)", Objects.hashCode(a));
+        System.out.printf(format1, "Objects.hashCode(b)", Objects.hashCode(b));
+        System.out.printf(format2, "Objects.equals(a, b)", Objects.equals(a, b));
+        System.out.printf(format2, "Objects.equals(a, c)", Objects.equals(a, c));
+        System.out.printf(format2, "Objects.equals(b, d)", Objects.equals(b, d));
+        System.out.printf(format3, "Objects.equals(first, second)", Objects.equals(first, second));
+        System.out.printf(format3, "Objects.deepEquals(first, second)", Objects.deepEquals(first, second));
+        System.out.printf(format3, "Arrays.equals(first, second)", Arrays.equals(first, second));
+        System.out.printf(format3, "Arrays.deepEquals(first, second)", Arrays.deepEquals(first, second));
+        System.out.printf(format4, "Objects.hashCode(first), Objects.hashCode(second)", Objects.hashCode(first), Objects.hashCode(second));
+        System.out.printf(format4, "Arrays.hashCode(first), Arrays.hashCode(second)", Arrays.hashCode(first), Arrays.hashCode(second));
     }
 }
