05d466a0867b1209cfeed0d64ff46fc0557bfcd8
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-07 14:59:09 +0000

    Add stripLeading and stripTrailing to StripSample and a way to remove indent for all lines in a string

--- src/main/java/com/sanver/basics/strings/StripSample.java
+++ src/main/java/com/sanver/basics/strings/StripSample.java
@@ -15,6 +15,8 @@ public class StripSample {
      * <p><b>strip():</b> Removes all types of whitespace characters according to the Unicode standard,
      * which includes a much broader set of whitespace characters (e.g., non-breaking spaces and other Unicode
      * whitespace characters).</p>
+     * <p><b>stripLeading():</b> Removes all types of whitespace characters from the beginning of the String. There is no trim equivalent for this.</p>
+     * <p><b>stripTrailing():</b> Removes all types of whitespace characters from the end of the String. There is no trim equivalent for this.</p>
      *
      * <h3>Introduction Version:</h3>
      * <p><b>trim():</b> Available since Java 1.0.</p>
@@ -51,5 +53,31 @@ public class StripSample {
         System.out.println("Original string with Unicode spaces: '" + unicodeSpaces + "'");
         System.out.println("After trim() : '" + trimmedUnicode + "'");
         System.out.println("After strip(): '" + strippedUnicode + "'");
+
+        System.out.println("unicodeSpaces.stripLeading() : " + unicodeSpaces.stripLeading());
+        System.out.println("unicodeSpaces.stripTrailing(): " + unicodeSpaces.stripTrailing());
+
+        var message = """
+                First Line
+                   Second Line
+                        Third Line
+                Last Line  
+                """;
+        System.out.println(message);
+        System.out.println(removeIndentation(message));
+    }
+
+    /**
+     * Removes indentation from the given string for each line by using <code>String.replaceAll("(?m)^\\s+", "")</code>.
+     * <ul>
+     * <li><code>(?m)</code>: Enables multiline mode, so <code>^</code> matches the start of each line (not just the start of the whole string).</li>
+     * <li><code>^</code>: Matches the beginning of each line.</li>
+     * <li><code>\\s+</code>: Matches one or more whitespace characters.</li>
+     * </ul>
+     * @param input a String indentation of which will be removed for each line
+     * @return indentation removed String
+     */
+    public static String removeIndentation(String input) {
+        return input.replaceAll("(?m)^\\s+", "");
     }
 }
