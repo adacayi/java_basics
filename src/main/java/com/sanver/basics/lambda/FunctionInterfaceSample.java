f7988bfe1cd3228cc907ac8ec332712e41a94932
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-07 16:07:47 +0000

    Refactor FunctionInterfaceSample

--- src/main/java/com/sanver/basics/lambda/FunctionInterfaceSample.java
+++ src/main/java/com/sanver/basics/lambda/FunctionInterfaceSample.java
@@ -2,35 +2,34 @@ package com.sanver.basics.lambda;
 
 import java.time.LocalDate;
 import java.util.function.Function;
-import java.util.function.IntFunction;;
+import java.util.function.IntFunction;
 
 public class FunctionInterfaceSample {
 
-	class Person {
-		private int birthDate;
+    static class Person {
+        private int birthDate;
 
-		public Person(int birthDate) {
-			this.birthDate = birthDate;
-		}
+        public Person(int birthDate) {
+            this.birthDate = birthDate;
+        }
 
-		public int getAge(Function<Integer, Integer> calculate) {
-			return calculate.apply(birthDate);
-		}
+        public int getAge(Function<? super Integer, ? extends Integer> calculate) { // It might be better to use IntUnaryOperator here. We used this just for Function interface demonstration.
+            return calculate.apply(birthDate);
+        }
 
-		public <T> T getAge(IntFunction<T> calculate) {
-			return calculate.apply(birthDate);
-		}
-	}
+        public <T> T getAge(IntFunction<T> getAgeInfo) {
+            return getAgeInfo.apply(birthDate);
+        }
+    }
 
-	public static void main(String[] args) {
-		FunctionInterfaceSample sample = new FunctionInterfaceSample();
-		Person Ahmet = sample.new Person(2010);
-		int currentYear = LocalDate.now().getYear();
-		Function<Integer, Integer> calculate = birthDate -> currentYear - birthDate;
-		IntFunction<String> calculateInt = birthDate -> "Your age is " + (currentYear - birthDate);
+    public static void main(String[] args) {
+        var person = new Person(2010);
+        int currentYear = LocalDate.now().getYear();
+        Function<Number, Integer> calculateAge = birthDate -> currentYear - birthDate.intValue();
+        IntFunction<String> getAgeInfo = birthDate -> "Your age is " + calculateAge.apply(birthDate);
 
-		System.out.println("Current year is " + currentYear);
-		System.out.println(Ahmet.getAge(calculate));
-		System.out.println(Ahmet.getAge(calculateInt));
-	}
+        System.out.println("Current year is " + currentYear);
+        System.out.println(person.getAge(calculateAge));
+        System.out.println(person.getAge(getAgeInfo));
+    }
 }
