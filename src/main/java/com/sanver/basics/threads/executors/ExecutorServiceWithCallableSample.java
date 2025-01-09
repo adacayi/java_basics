b4859166bf5d9c04c18db854f7214d470fbf9ded
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-09 11:40:43 +0000

    Use try-with-resources for the ExecutorService in ExecutorServiceWithCallableSample

--- src/main/java/com/sanver/basics/threads/executors/ExecutorServiceWithCallableSample.java
+++ src/main/java/com/sanver/basics/threads/executors/ExecutorServiceWithCallableSample.java
@@ -1,27 +1,27 @@
 package com.sanver.basics.threads.executors;
 
-import static com.sanver.basics.utils.Utils.sleep;
-
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static com.sanver.basics.utils.Utils.sleep;
 
 public class ExecutorServiceWithCallableSample {
 
-  public static void main(String[] args) throws ExecutionException, InterruptedException {
-    int[] value = new int[1];
-    var service = Executors.newFixedThreadPool(2);
-    Callable<int[]> increment = () -> {
-      sleep(3000);// This is put to show that Future.get() method waits for the call method to finish
-      value[0] = 1000;
-      return value;
-    };
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int[] value = new int[1];
+        try (var service = Executors.newFixedThreadPool(2)) {
+            Callable<int[]> increment = () -> {
+                sleep(3000);// This is put to show that Future.get() method waits for the call method to finish
+                value[0] = 1000;
+                return value;
+            };
 
-    var future = service.submit(increment);
+            Future<int[]> future = service.submit(increment);
 
-    System.out.println(value[0]);
-    System.out.println(future.get()[0]);
-
-    service.shutdown();
-  }
+            System.out.println(value[0]);
+            System.out.println(future.get()[0]);
+        }
+    }
 }
