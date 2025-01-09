d9a7b2d64719f829d5b2090cec6344e913169c03
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-09 15:01:38 +0000

    Print isDaemon and adjust output intervals in ThreadFactorySample

--- src/main/java/com/sanver/basics/threads/executors/ThreadFactorySample.java
+++ src/main/java/com/sanver/basics/threads/executors/ThreadFactorySample.java
@@ -7,26 +7,29 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntUnaryOperator;
 
+import static com.sanver.basics.utils.Utils.getThreadInfo;
 import static com.sanver.basics.utils.Utils.printThreadPool;
 import static com.sanver.basics.utils.Utils.sleep;
 
 public class ThreadFactorySample {
     public static void main(String[] args) {
         // thenApply uses the same executor as the previous CompletableFuture, whereas thenApplyAsync uses the ForkJoinPool.commonPool.
-        var executor = (ThreadPoolExecutor)Executors.newFixedThreadPool(2, new CustomThreadFactory("Custom-pool-worker"));
+        var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2, new CustomThreadFactory("Custom-pool-worker"));
+        printThreadPool(executor, "Initial state");
+        sleep(7000);
 
         IntUnaryOperator square = x -> {
-            var thread = Thread.currentThread();
-            System.out.printf("Calculation of square of %3d  started. Thread id: %3d, Thread name: %s%n",x, thread.getId(), thread.getName());
-            sleep(2000);
-            System.out.printf("Calculation of square of %3d finished. Thread id: %3d, Thread name: %s%n",x, thread.getId(), thread.getName());
+            System.out.printf("Calculation of square of %3d  started. %s%n", x, getThreadInfo());
+            sleep(7_000);
+            System.out.printf("Calculation of square of %3d finished. %s%n", x, getThreadInfo());
             return x * x;
         };
 
         var future = CompletableFuture.supplyAsync(() -> square.applyAsInt(3), executor).thenApply(square::applyAsInt);
         future.join();
 
-        printThreadPool(executor, "State of the Custom pool after all operations finished");
+        sleep(7_000);
+        printThreadPool(executor, "%nState of the custom pool after all operations finished".formatted());
         // Even though the pool size is not zero (there are still threads (not active though)),
         // since the threads are set as daemon,
         // the main thread will exit without calling the executor.shutdown()
