8cd454351ad53c73902ac161711781e10d9334ee
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-09 11:37:27 +0000

    Add comments for the type of Future returned by submit method for ExecutorService and refactor ExecutorServiceSample.

--- src/main/java/com/sanver/basics/threads/executors/ExecutorServiceSample.java
+++ src/main/java/com/sanver/basics/threads/executors/ExecutorServiceSample.java
@@ -1,9 +1,8 @@
 package com.sanver.basics.threads.executors;
 
-import java.util.concurrent.ExecutionException;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import static com.sanver.basics.utils.Utils.getThreadInfo;
 import static com.sanver.basics.utils.Utils.printThreadPool;
@@ -11,9 +10,9 @@ import static com.sanver.basics.utils.Utils.sleep;
 
 public class ExecutorServiceSample {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(1); // This is a ThreadPoolExecutor
-        printThreadPool((ThreadPoolExecutor)service, "Initial pool state");
+        printThreadPool(service, "Initial pool state");
         Runnable writeA = () -> {
             System.out.println("writeA started. " + getThreadInfo());
             sleep(5000);// Event though first runnable object sleeps for a while second runnable object
@@ -32,16 +31,20 @@ public class ExecutorServiceSample {
             }
         };
 
-        service.execute(writeA); // We can use the submit method instead to get back a Future<?>, which we can use to join. We can also use invokeAll, which blocks the code until the tasks are executed.
+        service.execute(writeA); // We can use the submit method instead to get back a Future<?>.
+//        // Future<?> is returned when the Runnable overload is invoked.
+//        // Future<Void> is returned when the Callable overload is invoked and the callable return value is null.
+//        // We can also use invokeAll to invoke a Collection of Callable, which blocks the code until the tasks are executed and will return List<Future<Object>>.
         service.execute(writeB);
 
-        printThreadPool((ThreadPoolExecutor)service, "Pool state after tasks were submitted");
+        printThreadPool(service, "Pool state after tasks were submitted");
 
         sleep(100);
         service.shutdown();
         System.out.println("Shutdown called");
-        // For ThreadPoolExecutor, shutdown does not interrupt the active tasks (use shutdownNow instead. shutdown lets all submitted tasks to execute, while shutdownNow will not execute tasks that are submitted but not started execution yet).
-        // For ThreadPoolTaskExecutor shutdown interrupts the active tasks.
+        printThreadPool(service, "Pool state after shutdown called");
+        // For ThreadPoolExecutor, shutdown does not interrupt the active tasks (use shutdownNow instead. shutdown lets all submitted tasks to execute including the ones in the queue, while shutdownNow will not execute tasks that are submitted but not started execution yet. shutdownNow will also try to interrupt already started tasks. Check AwaitTerminationShutdownSample.).
+        // For ThreadPoolTaskExecutor shutdown interrupts the active tasks and remove tasks from the queue and won't execute them.
         // If the thread pool is not shutdown, the main thread won't end, because the thread pool doesn't let the main thread exit
         // until all active threads are finished and pool size becomes zero
         // (meaning there should be no threads left in the pool.
