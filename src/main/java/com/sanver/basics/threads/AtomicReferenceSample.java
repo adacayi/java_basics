1a44defd418ce66bad144cb2ed61bae350ae475b
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-09 17:57:51 +0000

    Add case not using AtomicReference case, fix comments and refactor in AtomicReferenceSample

--- src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
+++ src/main/java/com/sanver/basics/threads/AtomicReferenceSample.java
@@ -16,31 +16,48 @@ import java.util.stream.IntStream;
  */
 public class AtomicReferenceSample {
 
+    private static IntValue shared = new IntValue(0);
+
     public static void main(String[] args) {
-        var atomicReference = new AtomicReference<>(new IntValue(0));
-        var futures = IntStream.range(0, 10).mapToObj(x -> CompletableFuture.runAsync(() -> increment(atomicReference))).toArray(CompletableFuture[]::new);
+        var numberFormat = NumberFormat.getInstance();
+        var futures = IntStream.range(0, 10).mapToObj(x -> CompletableFuture.runAsync(() -> increment())).toArray(CompletableFuture[]::new);
         CompletableFuture.allOf(futures).join();
-        System.out.println("Result after compare and set: " + NumberFormat.getInstance().format(atomicReference.get().getValue()));
+        System.out.printf("Result without atomic reference: %,d%n", shared.getValue());
+
+        shared = new IntValue(0);
+        var atomicReference = new AtomicReference<>(shared);
+        futures = IntStream.range(0, 10).mapToObj(x -> CompletableFuture.runAsync(() -> increment(atomicReference))).toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures).join();
+        System.out.println("Result with AtomicReference using compareAndSet: " + numberFormat.format(atomicReference.get().getValue()));
 
         Runnable accumulate = () -> {
             for (int i = 0; i < 100_000; i++) {
                 atomicReference.accumulateAndGet(new IntValue(4), (prev, x) -> new IntValue(prev.getValue() + x.getValue()));
-                // This tries to execute the binary operator and sets the value to the atomic reference if the previous value at the start of the operation remained the same. If not, it will retry with the current value until it is successful. So no need for a loop here, contrary to the case in compareAndSet.
-                // Important: Make sure that the binary operator does not change the prev object; otherwise, it will affect the check whether the previous value remained the same or not when assigning the final value calculated by the binary operator, hence might not be thread safe in the end.
-                // For example, this might not result in the desired sum: atomicReference.accumulateAndGet(new IntValue(4), (prev, x) -> prev.increment(x)) because increment method in IntValue changes the existing object. If it was return new IntValue(value + increment.getValue()), that would work correctly.
+                // This executes the binary operator and sets the value to the atomic reference if the previous value at the start of the operation remained the same (reference equality).
+                // If not, it will set the previous value to current value and apply the binary operator again until it is successful.
+                // So no need for a loop here, contrary to the case in compareAndSet.
+                // Important: Make sure that the binary operator returns an object not equal (!=) to the prev object, otherwise even though its properties might have changes, since the reference is the same,
+                // other threads concurrently changing the same value will think no change has occurred after they read the initial value, so they will apply their change to an older data, resulting in data loss.
+                // For example, try this, and you  might not see the desired sum: atomicReference.accumulateAndGet(new IntValue(4), (prev, x) -> prev.increment(x)) because prev.increment() is equal to prev.
             }
         };
 
         futures = IntStream.range(0, 10).mapToObj(x -> CompletableFuture.runAsync(accumulate)).toArray(CompletableFuture[]::new);
         CompletableFuture.allOf(futures).join();
-        System.out.println("Result after accumulating to compare and set value: " + NumberFormat.getInstance().format(atomicReference.get().getValue()));
+        System.out.println("Result after accumulating to compare and set value: " + numberFormat.format(atomicReference.get().getValue()));
     }
 
-    public static void increment(AtomicReference<IntValue> value) {
+    private static void increment() {
+        for (int i = 0; i < 100_000; i++) {
+            shared = new IntValue(shared.getValue() + 3);
+        }
+    }
+
+    private static void increment(AtomicReference<IntValue> value) {
         for (int i = 0; i < 100_000; i++) {
             boolean incremented = false;
 
-            while (!incremented) { // If this while block did not exist, the final value will be less than 10,000.
+            while (!incremented) { // If this while block did not exist, the final value will be less than 3,000,000.
                 IntValue existingValue = value.get();
                 IntValue newValue = new IntValue(existingValue.getValue() + 3);
                 // This will try to set the AtomicReference value to the new value, if the current value is the same object
