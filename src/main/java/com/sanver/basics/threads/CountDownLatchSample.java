c63b06d149b4b650aa53e376f379c2237b201ee4
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-10 11:30:50 +0000

    Re-interrupt the current thread after catching InterruptedException in CountDownLatchSample

--- src/main/java/com/sanver/basics/threads/CountDownLatchSample.java
+++ src/main/java/com/sanver/basics/threads/CountDownLatchSample.java
@@ -1,30 +1,30 @@
 package com.sanver.basics.threads;
 
-import static com.sanver.basics.utils.Utils.sleep;
-
 import java.util.concurrent.CountDownLatch;
+
+import static com.sanver.basics.utils.Utils.sleep;
 
 public class CountDownLatchSample {
 
-  public static void main(String[] args) {
-    var count = 3;
-    var countDownLatch = new CountDownLatch(count);
-    var thread1 = new Thread(() -> {
-      try {
-        System.out.println("Count down started with " + countDownLatch.getCount());
-        countDownLatch.await();
-        System.out.println("Count down finished");
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    });
+    public static void main(String[] args) {
+        var count = 3;
+        var countDownLatch = new CountDownLatch(count);
+        var thread1 = new Thread(() -> {
+            try {
+                System.out.println("Count down started with " + countDownLatch.getCount());
+                countDownLatch.await();
+                System.out.println("Count down finished");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
 
-    thread1.start();
+        thread1.start();
 
-    for (int i = 0; i < count; i++) {
-      sleep(3000);
-      countDownLatch.countDown();
-      System.out.println("Count down " + countDownLatch.getCount());
+        for (int i = 0; i < count; i++) {
+            sleep(3000);
+            countDownLatch.countDown();
+            System.out.println("Count down " + countDownLatch.getCount());
+        }
     }
-  }
 }
