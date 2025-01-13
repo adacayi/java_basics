9c3015618c427733e33e9034f55a295e512738b2
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-13 17:59:48 +0000

    Refactor ThreadLocalSample and use threadId() instead of getId() for the thread id.

--- src/main/java/com/sanver/basics/threads/ThreadLocalSample.java
+++ src/main/java/com/sanver/basics/threads/ThreadLocalSample.java
@@ -1,36 +1,40 @@
 package com.sanver.basics.threads;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ThreadLocalSample {
     public static final ThreadLocal<String> traceId = new ThreadLocal<>();
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.printf("Main started with thread id %s. Trace id not set yet.%n", Thread.currentThread().getId());
+    public static void main(String[] args) {
+        System.out.printf("Main started with thread id %s. Trace id not set yet.%n", threadId());
         printContext();
         traceId.set("12345");
-        System.out.printf("Trace id set for main thread %s.%n", Thread.currentThread().getId());
+        System.out.printf("Trace id set for main thread %s.%n", threadId());
         printContext();
-        var thread1 = new Thread(getRunnable());
-        thread1.start();
-        thread1.join();
+        CompletableFuture.runAsync(getRunnable()).join();
         printContext();
         traceId.remove();
-        System.out.printf("Trace id removed for main thread %s.%n", Thread.currentThread().getId());
+        System.out.printf("Trace id removed for main thread %s.%n", threadId());
         printContext();
     }
 
+    private static long threadId() {
+        return Thread.currentThread().threadId();
+    }
+
     private static void printContext() {
-        System.out.printf("Thread: %s Trace id: %s.%n", Thread.currentThread().getId(), traceId.get());
+        System.out.printf("Thread: %s Trace id: %s.%n%n", threadId(), traceId.get());
     }
 
     public static Runnable getRunnable() {
         return () -> {
-            System.out.printf("New runnable started with thread id %s. Trace id not set yet.%n", Thread.currentThread().getId());
+            System.out.printf("New runnable started with thread id %s. Trace id not set yet.%n", threadId());
             // Note, as can be seen when the below code executed, traceId will be null, since this is a new thread.
             printContext();
             traceId.set("abc");
-            System.out.printf("Trace id set for thread %s.%n", Thread.currentThread().getId());
+            System.out.printf("Trace id set for thread %s.%n", threadId());
             printContext();
-            System.out.printf("Trace id removed for thread %s.%n", Thread.currentThread().getId());
+            System.out.printf("Trace id removed for thread %s.%n", threadId());
             traceId.remove();
             printContext();
         };
