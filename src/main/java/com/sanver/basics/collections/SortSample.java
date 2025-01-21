f0d4cacb9cb7e022ba9f0822e487bb2a1c802b5b
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-21 16:51:56 +0000

    Add list.sort(null) for SortSample and refactor

--- src/main/java/com/sanver/basics/collections/SortSample.java
+++ src/main/java/com/sanver/basics/collections/SortSample.java
@@ -1,27 +1,32 @@
 package com.sanver.basics.collections;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class SortSample {
 
-	public static void main(String[] args) {
-		List<Integer> values = new ArrayList<>(Arrays.asList(4, 2, 3, 5));
-		Comparator<Integer> ascending = Comparator.naturalOrder();
-		Comparator<Integer> descending = ascending.reversed();
-		values.sort(ascending);
-		System.out.println(values);
-		Collections.sort(values); // This can also be used for sorting
-		// Collections.reverse(values) should not be used to sort in descending order.
-		// Instead comparators should be used.
-		values.sort(descending);
-		System.out.println(values);
-		// This is another alternative
-		Collections.sort(values, descending);
-		System.out.println(values);
-	}
+    public static void main(String[] args) {
+        var values = new ArrayList<>(Arrays.asList(4, 2, 3, 5));
+        Comparator<Integer> ascending = Comparator.naturalOrder();
+        Comparator<Integer> descending = ascending.reversed();
+        var format = "%-37s: %s%n";
+        values.sort(ascending);
+        System.out.printf(format, "values.sort(ascending)", values);
 
+        values.sort(descending);
+        System.out.printf(format, "values.sort(descending)", values);
+
+        Collections.sort(values); // This can also be used for sorting
+        // Collections.reverse(values) should not be used to sort in descending order.
+        // Instead, comparators should be used.
+        System.out.printf(format, "Collections.sort(values)", values);
+
+        Collections.sort(values, descending);
+        System.out.printf(format, "Collections.sort(values, descending)", values);
+
+        values.sort(null); // This is what Collections.sort(list) does
+        System.out.printf(format, "values.sort(null)", values); // sort(null) sorts the list into ascending order according to the natural ordering of its elements
+    }
 }
