bfb571419d4338238f9b4a9908f56d645e7e8d7e
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-21 16:36:51 +0000

    Address warnings and refactor SortingWithMultipleFields

--- src/main/java/com/sanver/basics/collections/SortingWithMultipleFields.java
+++ src/main/java/com/sanver/basics/collections/SortingWithMultipleFields.java
@@ -4,53 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SortingWithMultipleFields {
 
-    static class Person {
-        String name;
-        String country;
-        int age;
-
-        public String getName() {
-            return name;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public Person(String name, String country, int age) {
-            this.name = name;
-            this.age = age;
-            this.country = country;
-        }
-
-        public String toString() {
-            return name + "  " + country + "  " + age;
-        }
-    }
+    public static final String AHMET = "Ahmet";
+    public static final String ENGLAND = "England";
 
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>(Arrays.asList(
-                new Person("Ahmet", "Turkiye", 1),
-                new Person("Ahmet", "England", 3),
-                new Person("Ahmet", "England", 2),
-                new Person("Mustafa", "England", 2),
-                new Person("Mustafa", "Turkiye", 5),
-                new Person("Mustafa", "Turkiye", 7),
-                new Person(null, null, -1)));
+                new Person("Turkiye", AHMET, 1),
+                new Person(ENGLAND, "AHMET", 3),
+                new Person(ENGLAND, AHMET, 2),
+                new Person("ENGLAND", "Mustafa", 2),
+                new Person("Turkiye", "Mustafa", 5),
+                new Person("TURKIYE", "MUSTAFA", 7),
+                new Person(null, AHMET, 12),
+                new Person(ENGLAND, null, 9),
+                new Person(null, null, 15)
+        ));
         // To sort with name descending, country ascending and age descending:
-        Comparator<String> stringComparator = Comparator.nullsFirst(String::compareToIgnoreCase);
-        Comparator<Person> nameDescending = Comparator.comparing(Person::getName, stringComparator).reversed();
-        Comparator<Person> ageDescending = Comparator.comparing(Person::getAge).reversed();
-        people.sort(nameDescending.thenComparing(Person::getCountry, stringComparator).thenComparing(ageDescending));
-        String content = people.stream().map(Person::toString).collect(Collectors.joining("\n"));
-        System.out.println(content);
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        Comparator<String> stringComparator = Comparator.nullsLast(compareToIgnoreCase.reversed());
+        Comparator<Person> nameDescending = Comparator.comparing(Person::name, stringComparator);
+        Comparator<Person> ageDescending = Comparator.comparing(Person::age).reversed();
+        people.sort(Comparator.comparing(Person::country, stringComparator).thenComparing(nameDescending).thenComparing(ageDescending));
+        IntStream.range(0, people.size()).forEach(i -> System.out.printf("%-2d- %-10s %-10s %-2d%n", i + 1, people.get(i).country, people.get(i).name, people.get(i).age));
+    }
+
+    record Person(String country, String name, int age) {
+
     }
 }
