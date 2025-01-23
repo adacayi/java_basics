9b0ec41952dff90eb9f2c544ed81bf3ee79b0bc4
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-23 18:46:15 +0000

    Add component type output for CheckingIfArrayIsMultidimensional

--- src/main/java/com/sanver/basics/arrays/CheckingIfArrayIsMultidimensional.java
+++ src/main/java/com/sanver/basics/arrays/CheckingIfArrayIsMultidimensional.java
@@ -9,15 +9,19 @@ public class CheckingIfArrayIsMultidimensional {
         Object[] array3 = {new Object[]{new Object(), new Object()}, new Object()};
         Object[] array4 = {new Object[]{new Object(), new Object()}};
         Object[][] array5 = {{new Object(), new Object()}};
-        var format = "%s is %s a multidimensional array%n";
-        System.out.printf(format, Arrays.toString(array1), isMultidimensional(array1) ? "" : "not");
-        System.out.printf(format, Arrays.deepToString(array2), isMultidimensional(array2) ? "" : "not");
-        System.out.printf(format, Arrays.deepToString(array3), isMultidimensional(array3) ? "" : "not");
-        System.out.printf(format, Arrays.deepToString(array4), isMultidimensional(array4) ? "" : "not"); // Even though the only element in array4 is an Object[], since  array4.getClass().getComponentType() returns Object, instead of Object[] isMultidimensional will return false. So, if you use this approach to figure out if an array is multidimensional or not and this returns false, it does not mean that it can contain arrays, as shown in this example.
-        System.out.printf(format, Arrays.deepToString(array5), isMultidimensional(array5) ? "" : "not");
+        var format = "%-84s is %-3s a multidimensional array. Component type: %s%n";
+        System.out.printf(format, Arrays.toString(array1), isMultidimensional(array1) ? "" : "not", getComponentType(array1));
+        System.out.printf(format, Arrays.deepToString(array2), isMultidimensional(array2) ? "" : "not", getComponentType(array2));
+        System.out.printf(format, Arrays.deepToString(array3), isMultidimensional(array3) ? "" : "not", getComponentType(array3));
+        System.out.printf(format, Arrays.deepToString(array4), isMultidimensional(array4) ? "" : "not", getComponentType(array4));
+        System.out.printf(format, Arrays.deepToString(array5), isMultidimensional(array5) ? "" : "not", getComponentType(array5));
     }
 
     public static boolean isMultidimensional(Object array) {
         return array.getClass().isArray() && array.getClass().getComponentType().isArray();
+    }
+
+    private static String getComponentType(Object array) {
+        return array.getClass().getComponentType().toString();
     }
 }
