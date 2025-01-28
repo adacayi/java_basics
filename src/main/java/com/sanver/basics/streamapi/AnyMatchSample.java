4761724b6f15b4e5fbae2014323321efd4bfef55
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-28 14:18:51 +0000

    Enhance AnyMatchSample with parallel stream example

--- src/main/java/com/sanver/basics/streamapi/AnyMatchSample.java
+++ src/main/java/com/sanver/basics/streamapi/AnyMatchSample.java
@@ -1,16 +1,27 @@
 package com.sanver.basics.streamapi;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AnyMatchSample {
 
-	public static void main(String[] args) {
-		int[] numbers = { 1, 2, 3, 4, 5 };
-		boolean result = Arrays.stream(numbers).anyMatch(x -> {
-			System.out.println("Checking if " + x + " satisfies condition");
-			return x > 2;
-		});
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        boolean result = Arrays.stream(numbers).anyMatch(x -> {
+            System.out.println("Checking if " + x + " satisfies condition x > 2");
+            return x > 2;
+        });
 
-		System.out.println(result);
-	}
+        System.out.println(result);
+
+        numbers = new int[100];
+        AtomicInteger count = new AtomicInteger(0);
+        Arrays.parallelSetAll(numbers, x -> x);
+        result = Arrays.stream(numbers).parallel().anyMatch(x -> {
+            System.out.println("Checking if " + x + " satisfies condition x > 10");
+            count.incrementAndGet();
+            return x > 10;
+        });
+        System.out.printf("Finding if any item exists in the int array from 0 to 99, that has an item > 10. Result: %s. Check count with parallel stream: %d%n", result, count.get());
+    }
 }
