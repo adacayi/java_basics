352be2166b33cdadc23fa86a8c63a31cd31b5d75
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-28 16:26:35 +0000

    Add example to demonstrate that filter does not filter all at once before moving to the next stream operation

--- src/main/java/com/sanver/basics/streamapi/FilterCollectSample.java
+++ src/main/java/com/sanver/basics/streamapi/FilterCollectSample.java
@@ -15,5 +15,17 @@ public class FilterCollectSample {
         odd = Arrays.stream(numbers).filter(x -> x % 2 == 1).toArray();
         System.out.println("Even numbers: " + Arrays.toString(even));
         System.out.println("Odd  numbers: " + Arrays.toString(odd));
+        System.out.println();
+
+        var result = IntStream.range(0, 10)
+                .filter(x -> {
+                    System.out.printf("Filtering if %d is divisible by 3: %s%n", x, x % 3 == 0);
+                    return x % 3 == 0;
+                }).map(x -> {
+                    System.out.printf("Mapping %d to %d%n", x, x / 3);
+                    return x / 3;
+                }).toArray();
+
+        System.out.printf("%nResult after filtering numbers between 0 to 9 by checking if they are divisible by 3 and mapping the values to 1/3: %s%n", Arrays.toString(result));
     }
 }
