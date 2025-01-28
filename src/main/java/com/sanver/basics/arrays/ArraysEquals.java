74a1a2b3a892911b6088bf0c22e91c38d783df9e
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-28 11:46:12 +0000

    Rename class CheckingArrayEquality to ArraysEquals for consistency with ArraysHashCode

--- src/main/java/com/sanver/basics/arrays/ArraysEquals.java
+++ src/main/java/com/sanver/basics/arrays/ArraysEquals.java
@@ -3,7 +3,7 @@ package com.sanver.basics.arrays;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class CheckingArrayEquality {
+public class ArraysEquals {
     public static void main(String[] args) {
         int[] first = IntStream.range(0, 10).toArray();
         int[] second = IntStream.range(0, 10).toArray();
