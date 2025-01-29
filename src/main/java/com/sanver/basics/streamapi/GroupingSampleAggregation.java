deed8150a0c7ca1498a7331d457e0990af9c3c82
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-29 13:00:47 +0000

    Refactor and simplify data classes in GroupingSampleAggregation

--- src/main/java/com/sanver/basics/streamapi/GroupingSampleAggregation.java
+++ src/main/java/com/sanver/basics/streamapi/GroupingSampleAggregation.java
@@ -1,103 +1,45 @@
 package com.sanver.basics.streamapi;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.Value;
 
-import java.util.Comparator;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GroupingSampleAggregation {
-    private static int lastId = 0;
-
-    static class Person {
-        private int id;
-        private String name;
-        private int age;
-        private String city;
-
-        public Person(String name, int age, String city) {
-            this.id = getNewId();
-            this.name = name;
-            this.age = age;
-            this.city = city;
-        }
-
-        public String toString() {
-            return String.format("%d- %s at age %d in %s", id, name, age, city);
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        private synchronized int getNewId() {
-            return ++lastId;
-        }
-    }
-
-    static class GroupKey {
-        private String name;
-        private String city;
-
-        public GroupKey(String name, String city) {
-            this.name = name;
-            this.city = city;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-
-            if (!(obj instanceof GroupKey)) {
-                return false;
-            }
-
-            var other = (GroupKey) obj;
-
-            return new EqualsBuilder()
-                    .append(name, other.name)
-                    .append(city, other.city)
-                    .isEquals();
-        }
-
-        @Override
-        public int hashCode() {
-            return new HashCodeBuilder()
-                    .append(name)
-                    .append(city)
-                    .hashCode();
-        }
-    }
-
     public static void main(String[] args) {
         int personCount = 16;
+        var random = new Random();
         var people = IntStream.rangeClosed(1, personCount).mapToObj(x ->
-                new Person(Math.random() > 0.5 ? "Abdullah" : "Hatice", (int) (Math.random() * 90 + 1), Math.random() > 0.5 ? "Istanbul" : "Urfa"))
-                .sorted(Comparator.comparing((Person x) -> x.name).thenComparing(x -> x.city)).collect(Collectors.toList());
-        System.out.println("People:");
+                new Person(Math.random() > 0.5 ? "Abdullah" : "Hatice", random.nextInt(90) + 1, random.nextBoolean() ? "Istanbul" : "Urfa")).toList();
+        System.out.printf("People%n%n");
 
         for (Person person : people) {
             System.out.println(person);
         }
-        var group = people.stream().collect(Collectors.groupingBy(p -> new GroupKey(p.getName(), p.getCity()), Collectors.averagingDouble(x -> x.age)));
 
-        System.out.println("Name city group:\n");
-        group.forEach((key, value) -> System.out.println(String.format("%-18s Average age = %.2f", key.name + " " + key.city, value)));
+        Map<GroupKey, Double> group = people.stream().collect(Collectors.groupingBy(p -> new GroupKey(p.getName(), p.getCity()), Collectors.averagingInt(x -> x.age)));
 
+        System.out.printf("%nName city groups%n%n");
+        var format = "[%-8s, %-8s]: Average age = %.2f%n";
+        group.forEach((key, value) -> System.out.printf(format, key.name, key.city, value));
+    }
+
+    @Value
+    private static class Person {
+        private static AtomicInteger lastId = new AtomicInteger(0);
+        int id = lastId.incrementAndGet();
+        String name;
+        int age;
+        String city;
+
+        public String toString() {
+            return String.format("[%2d, %-8s, %2d, %-8s]", id, name, age, city);
+        }
+    }
+
+    record GroupKey(String name, String city) {
     }
 }
