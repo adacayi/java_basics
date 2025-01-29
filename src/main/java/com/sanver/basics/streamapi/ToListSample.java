8f8db9035dc43d029f4eeaa0a53a91f8364bc888
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-29 17:54:43 +0000

    Use list.sort(null) for a more concise natural comparator usage when sorting a list

--- src/main/java/com/sanver/basics/streamapi/ToListSample.java
+++ src/main/java/com/sanver/basics/streamapi/ToListSample.java
@@ -1,7 +1,6 @@
 package com.sanver.basics.streamapi;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +9,10 @@ public class ToListSample {
         int[] array = {4, 1, 2, 3};
         List<Integer> list = Arrays.stream(array).boxed().toList(); // toList is available since Java 16. The returned List is unmodifiable; calls to any mutator method will always cause UnsupportedOperationException to be thrown.
         System.out.println(list);
-//        list.sort(Comparator.naturalOrder()); // This throws java.lang.UnsupportedOperationException, since toList() constructs an unmodifiable list.
+//        list.sort(null); // This throws java.lang.UnsupportedOperationException, since toList() constructs an unmodifiable list.
         List<Integer> list2 = Arrays.stream(array).boxed().collect(Collectors.toList());// This returns a modifiable list
         list2.add(5);
-        list2.sort(Comparator.naturalOrder());
+        list2.sort(null);
         System.out.println(list2);
     }
 }
