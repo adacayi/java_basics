6f2a8cf776cb8ecd4fb04f9932c1c5ff56c662a5
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-29 13:25:53 +0000

    Refactor and simplify GroupingSampleWithMultipleFields

--- src/main/java/com/sanver/basics/streamapi/GroupingSampleWithMultipleFields.java
+++ src/main/java/com/sanver/basics/streamapi/GroupingSampleWithMultipleFields.java
@@ -1,52 +1,25 @@
 package com.sanver.basics.streamapi;
 
-import lombok.Value;
-
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class GroupingSampleWithMultipleFields {
-    private static final Random random = new Random();
-    private static AtomicInteger lastId = new AtomicInteger(0);
+import static com.sanver.basics.streamapi.GroupingSampleAggregation.GroupKey;
+import static com.sanver.basics.streamapi.GroupingSampleAggregation.Person;
+import static com.sanver.basics.streamapi.GroupingSampleAggregation.generatePerson;
 
+public class GroupingSampleWithMultipleFields {
     public static void main(String[] args) {
         int personCount = 16;
-        var people = IntStream.rangeClosed(1, personCount).mapToObj(x -> generatePerson()).sorted(Comparator.comparing((Person x) -> x.name).thenComparing(x -> x.city).thenComparing(x -> x.age)).toList();
+        var people = IntStream.rangeClosed(1, personCount).mapToObj(x -> generatePerson()).sorted(Comparator.comparing(Person::getName).thenComparing(Person::getCity)).toList();
         people.forEach(System.out::println);
 
         var group = people.stream().collect(Collectors.groupingBy(p -> new GroupKey(p.getName(), p.getCity())));
         System.out.printf("%nName city group:%n%n");
-        var format = "%-8s %-8s: %s";
-        var entry = new ArrayList<>(group.entrySet());
-        entry.sort(Comparator.comparing((Map.Entry<GroupKey, List<Person>> x) -> x.getKey().name()).thenComparing(x -> x.getKey().city()));
-
-        entry.forEach(e -> System.out.println(String.format(format, e.getKey().name(), e.getKey().city(), e.getValue().stream().map(Person::toString).collect(Collectors.joining(", ")))));
-    }
-
-    private static Person generatePerson() {
-        String[] names = {"Abdullah", "Hatice"};
-        String[] cities = {"Istanbul", "Urfa"};
-        return new Person(names[random.nextInt(0, names.length)], random.nextInt(1, 91), cities[random.nextInt(0, cities.length)]);
-    }
-
-    @Value
-    private static class Person {
-        int id = lastId.incrementAndGet();
-        String name;
-        int age;
-        String city;
-
-        public String toString() {
-            return String.format("%2d- %-8s %-2d %-8s", id, name, age, city);
-        }
-    }
-
-    private record GroupKey(String name, String city) {
+        var format = "%-8s %-8s: %s%n";
+        var comparator = Comparator.comparing((Map.Entry<GroupKey, List<Person>> x) -> x.getKey().name()).thenComparing(x -> x.getKey().city());
+        group.entrySet().stream().sorted(comparator).forEach(e -> System.out.printf(format, e.getKey().name(), e.getKey().city(), e.getValue().stream().map(Person::toString).collect(Collectors.joining(", "))));
     }
 }
