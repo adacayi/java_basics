5a2b89dce73afde0a34f3f420f5384ca9e901b7f
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-30 16:42:30 +0000

    Added cases for data types long, char, float, and boolean, in both writing and reading operations in DataInputStreamSample

--- src/main/java/com/sanver/basics/io/streams/DataInputStreamSample.java
+++ src/main/java/com/sanver/basics/io/streams/DataInputStreamSample.java
@@ -5,46 +5,49 @@ import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DataInputStreamSample {
 
-	public static void main(String[] args) {
-		byte value1 = 127;
-		short value2 = 15431;
-		double value3 = 3.14159265;
-		double value4 = 2.23;
-		int value5 = 1821178321;
-		String fileName = "src/main/java/com/sanver/basics/io/streams/TrialWithNumbers.txt";
-		try (FileOutputStream stream = new FileOutputStream(fileName);
-				BufferedOutputStream buff = new BufferedOutputStream(stream);
-				DataOutputStream writer = new DataOutputStream(buff)) {
-			writer.writeByte(value1);
-			writer.writeShort(value2);
-			writer.writeDouble(value3);
-			writer.writeDouble(value4);
-			writer.writeInt(value5);
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-		try (FileInputStream stream = new FileInputStream(fileName);
-				BufferedInputStream buff = new BufferedInputStream(stream);
-				DataInputStream reader = new DataInputStream(buff)) {
-			System.out.println(reader.readByte());
-			System.out.println(reader.readShort());
-			System.out.println(reader.readDouble());
-			System.out.println(reader.readDouble());
-			System.out.println(reader.readInt());
-
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    public static void main(String[] args) {
+        byte value1 = 127;
+        short value2 = 15_431;
+        int value3 = 1_821_178_321;
+        long value4 = 1_000_000_000_000_000L;
+        char value5 = 'A';
+        float value6 = 2.23f;
+        double value7 = 3.14159265;
+        boolean value8 = true;
+        String fileName = "src/main/java/com/sanver/basics/io/streams/TrialWithNumbers.txt";
+        try (FileOutputStream stream = new FileOutputStream(fileName);
+             BufferedOutputStream buff = new BufferedOutputStream(stream);
+             DataOutputStream writer = new DataOutputStream(buff)) {
+            writer.writeByte(value1);
+            writer.writeShort(value2);
+            writer.writeInt(value3);
+            writer.writeLong(value4);
+            writer.writeChar(value5);
+            writer.writeFloat(value6);
+            writer.writeDouble(value7);
+            writer.writeBoolean(value8);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try (FileInputStream stream = new FileInputStream(fileName);
+             BufferedInputStream buff = new BufferedInputStream(stream);
+             DataInputStream reader = new DataInputStream(buff)) {
+            System.out.println(reader.readByte());
+            System.out.println(reader.readShort());
+            System.out.println(reader.readInt());
+            System.out.println(reader.readLong());
+            System.out.println(reader.readChar());
+            System.out.println(reader.readFloat());
+            System.out.println(reader.readDouble());
+            System.out.println(reader.readBoolean());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
