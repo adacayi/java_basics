4bb57da01e672045b735b60b42067f8fae848d5a
Abdullah Sanver <a_sanver@hotmail.com>
2025-01-31 12:26:52 +0000

    Set System.in to original System.in and use Scanner to read it in WritingDataToSystemIn

--- src/main/java/com/sanver/basics/io/streams/WritingDataToSystemIn.java
+++ src/main/java/com/sanver/basics/io/streams/WritingDataToSystemIn.java
@@ -1,27 +1,31 @@
 package com.sanver.basics.io.streams;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class WritingDataToSystemIn {
 
-	public static void main(String[] args) {
-		String message = "Selamunaleykum";
-		InputStream stream = new ByteArrayInputStream((message + "\n").getBytes());
-		// InputStream oldStream = System.in;
-		System.setIn(stream);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));// It is neccessary to set the
-																						// reader after changing
-																						// System.in. Else it will still
-																						// read from the old stream.
-
-		try {
-			System.out.println(reader.readLine());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        var message = """
+                Writer line 1
+                Writer line 2
+                
+                Writer line 4
+                
+                """;
+        InputStream stream = new ByteArrayInputStream((message).getBytes());
+        final InputStream originalSystemIn = System.in;
+        try {
+            System.setIn(stream);
+            try (var scanner = new Scanner(System.in)) {// It is necessary to set the scanner after changing System.in. Else it will still read from the old stream.
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+            }
+        } finally {
+            System.out.println("Finished");
+            System.setIn(originalSystemIn);
+        }
+    }
 }
