5659621e4ab69079d1ed80241c269f025f7a6b29
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-04 12:08:16 +0000

    Add comments to explain behavior for paths beginning with "/" and "./" to PathSample

--- src/main/java/com/sanver/basics/nio/PathSample.java
+++ src/main/java/com/sanver/basics/nio/PathSample.java
@@ -12,22 +12,30 @@ public class PathSample {
         //combining strings to a path
         var basePath = Path.of("src", "main", "java", "com", "sanver", "basics", "nio"); // We can also use Paths.get() introduced in Java 7, but Path.of introduced in Java 11 is more intuitive because it’s a simpler, more direct factory method, aligning with other factory methods introduced in recent Java versions, like List.of(), Set.of(), etc.
         var baseDirectory = "src/main/java/com/sanver/basics/nio/";
-        // These also work
-        // "./src/main/java/com/sanver/basics/nio/"
-        // "../basics/src/main/java/com/sanver/basics/nio/"
-        // This doesn't work
-        // "/src/main/java/com/sanver/basics/nio/"
+        // These also point to the same base path:
+
+        // Path.of("./src/main/java/com/sanver/basics/nio/") // The single dot at the beginning has no effect in here since in file paths, "./" represents the current directory, but Java's Path.of() method normalizes this automatically, treating "./src/..." the same as "src/..."
+        // Path.of("../basics/src/main/java/com/sanver/basics/nio/")
+
+        // Below does not point to [current working directory]\src\main\.., it points to D:\src\main\... instead. Call toAbsolutePath() to check.
+
+        // Path.of("/src/main/java/com/sanver/basics/nio/")
+        // This is because paths beginning with "/" are treated as absolute paths from the root directory, not relative to the current working directory.
+        // However, isAbsolute will still return false for Windows based systems, since Windows has a different format for absolute paths that includes a drive letter (like "C:").
+        // This behavior is a notable platform-specific detail that developers should be aware of when working with paths in Java, especially in cross-platform applications.
+
         var fileName = PathSample.class.getSimpleName() + ".java";
         Path filePath = Path.of(baseDirectory, fileName);
         Path directoryPath = Path.of(baseDirectory);
         File file = new File(fileName);
-        var absolutePathString = file.getAbsolutePath();// To get absolute path as path we can use Path.of(file.getAbsolutePath()) or Paths.get(file.getAbsolutePath())
+        var absolutePathString = file.getAbsolutePath();// To get absolute path as path we can use file.toPath().toAbsolutePath(), Path.of(file.getAbsolutePath()) or Paths.get(file.getAbsolutePath())
         Path absolutePath = Path.of(file.getAbsolutePath());
         System.out.println("baseDirectory: " + baseDirectory);
         System.out.println("fileName: " + fileName);
         System.out.println("new File(fileName) " + file);
         System.out.println("file.getAbsolutePath(): " + absolutePathString);
         System.out.println("absolutePath = Path.of(file.getAbsolutePath()): " + absolutePath);
+        System.out.println("file.toPath().toAbsolutePath()): " + file.toPath().toAbsolutePath());
         System.out.println("absolutePath.isAbsolute(): " + absolutePath.isAbsolute());
         System.out.println("filePath = Path.of(baseDirectory, fileName): " + filePath);
         System.out.println("filePath.isAbsolute(): " + filePath.isAbsolute());
@@ -69,9 +77,9 @@ public class PathSample {
                         .resolve(directoryPath.getParent().getParent().relativize(filePath)));
         try (var stream = Files.list(basePath.getParent())) { // It is suggested that we use try-with-resources with this stream
             System.out.println("Files.isSameFile(filePath, filePath.toAbsolutePath()): "
-                    + Files.isSameFile(filePath, filePath.toAbsolutePath()));
+                    + Files.isSameFile(filePath, filePath.toAbsolutePath())); // Tests if two paths locate the same file.
             System.out.println(stream.map(p -> p.getFileName().toString())
-                    .collect(Collectors.joining(", ", "Files and directories in %s: ".formatted(basePath.getParent()), "")));// This will print files and folders, but not content of folders and its subfolders. For that we need Files.walk
+                    .collect(Collectors.joining(", ", "Files and directories in %s:%n".formatted(basePath.getParent()), "")));// This will print files and folders, but not content of folders and its subfolders. For that we need Files.walk
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
