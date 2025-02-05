b03a5c8a53601f57b18ff7078d3de84af5da811a
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-05 16:21:59 +0000

    Refactor PeriodSample and add Period.ofDays, Period.ofWeeks, Period.ofMonths, and Period.ofYears samples

--- src/main/java/com/sanver/basics/time/PeriodSample.java
+++ src/main/java/com/sanver/basics/time/PeriodSample.java
@@ -28,47 +28,56 @@ public class PeriodSample {
 
     public static void main(String[] args) {
         // Example 1: Creating Periods
-        var period1 = Period.of(2, 3, 15);  // 2 years, 3 months, 15 days
-        System.out.println("period1: " + period1);
+        var period1 = Period.of(2, 3, 15);
+        var format = "%-50s : %s%n";
+        System.out.printf(format, "Period.ofDays(5)", Period.ofDays(5));
+        System.out.printf(format, "Period.ofWeeks(58)", Period.ofWeeks(58)); // This is the same as Period.ofDays(Math.multiplyExact(58, 7)), resulting in a period with days only.
+        System.out.printf(format, "Period.ofMonths(3)", Period.ofMonths(3));
+        System.out.printf(format, "Period.ofYears(10)", Period.ofYears(10));
+        System.out.printf(format, "period1 = Period.of(2, 3, 15)", period1);
 
         // Example 2: Calculating Period Between Two Dates
         var startDate = LocalDate.of(2020, 2, 29);
         var endDate = LocalDate.of(2023, 2, 28);
         var periodBetween = Period.between(startDate, endDate);
-        System.out.println("startDate: " + startDate);
-        System.out.println("endDate  : " + endDate);
-        System.out.println("periodBetween = Period.between(startDate, endDate): " + Period.between(startDate, endDate));
-        System.out.println("periodBetween.getYears(): " + periodBetween.getYears());
-        System.out.println("periodBetween.getMonths(): " + periodBetween.getMonths());
-        System.out.println("ChronoUnit.MONTHS.between(startDate, endDate): " + ChronoUnit.MONTHS.between(startDate, endDate));
-        System.out.println("periodBetween.getDays(): " + periodBetween.getDays());
-        System.out.println("ChronoUnit.DAYS.between(startDate, endDate): " + ChronoUnit.DAYS.between(startDate, endDate));
-        System.out.println("periodBetween.getUnits(): " + periodBetween.getUnits());
+        System.out.printf(format, "startDate = LocalDate.of(2020, 2, 29)", startDate);
+        System.out.printf(format, "endDate = LocalDate.of(2023, 2, 28)", endDate);
+        System.out.printf(format, "periodBetween = Period.between(startDate, endDate)", Period.between(startDate, endDate));
+        System.out.printf(format, "periodBetween.getYears()", periodBetween.getYears());
+        System.out.printf(format, "periodBetween.getMonths()", periodBetween.getMonths());
+        System.out.printf(format, "ChronoUnit.MONTHS.between(startDate, endDate)", ChronoUnit.MONTHS.between(startDate, endDate));
+        System.out.printf(format, "periodBetween.getDays()", periodBetween.getDays());
+        System.out.printf(format, "ChronoUnit.DAYS.between(startDate, endDate)", ChronoUnit.DAYS.between(startDate, endDate));
+        System.out.printf(format, "periodBetween.getUnits()", periodBetween.getUnits());
 
         // Example 3: Adding a Period to a Date
-        System.out.println("startDate.plus(period1): " +  startDate.plus(period1));
+        System.out.printf(format, "startDate", startDate);
+        System.out.printf(format, "period1", period1);
+        System.out.printf(format, "startDate.plus(period1)", startDate.plus(period1));
 
         // Example 4: Subtracting a Period from a Date
-        System.out.println("endDate.minus(period1): " + endDate.minus(period1));
+        System.out.printf(format, "endDate", endDate);
+        System.out.printf(format, "period1", period1);
+        System.out.printf(format, "endDate.minus(period1)", endDate.minus(period1));
 
         // Example 5: Checking Period properties
-        System.out.println("period1.isZero(): " + period1.isZero());
-        System.out.println("period1.isNegative(): " + period1.isNegative());
+        System.out.printf(format, "period1.isZero()", period1.isZero());
+        System.out.printf(format, "period1.isNegative()", period1.isNegative());
 
         // Example 6: Adding and Subtracting Periods
         Period period2 = Period.of(1, 2, 10);  // 1 year, 2 months, 10 days
-        System.out.println("period1: " + period1);
-        System.out.println("period2: " + period2);
-        System.out.println("period1.plus(period2): " + period1.plus(period2)); // Note that Period is immutable. period1 is not changed after invoking this method.
-        System.out.println("period1.minus(period2): " + period1.minus(period2));
-        System.out.println("period1.minusYears(5): " + period1.minusYears(5));
-        System.out.println("period1.plusMonths(20): " + period1.plusMonths(20));
-        System.out.println("period1.minusDays(16): " + period1.minusDays(16));
+        System.out.printf(format, "period1 = Period.of(2, 3, 15)", period1);
+        System.out.printf(format, "period2 = Period.of(1, 2, 10)", period2);
+        System.out.printf(format, "period1.plus(period2)", period1.plus(period2)); // Note that Period is immutable. period1 is not changed after invoking this method.
+        System.out.printf(format, "period1.minus(period2)", period1.minus(period2));
+        System.out.printf(format, "period1.minusYears(5)", period1.minusYears(5));
+        System.out.printf(format, "period1.plusMonths(20)", period1.plusMonths(20));
+        System.out.printf(format, "period1.minusDays(16)", period1.minusDays(16));
 
         // Example 7: Changing a part of the period
-        System.out.println("period1.withYears(19): " + period1.withYears(19));
-        System.out.println("period1.withMonths(7): " + period1.withMonths(7));
-        System.out.println("period1.withDays(31): " + period1.withDays(31));
+        System.out.printf(format, "period1.withYears(19)", period1.withYears(19));
+        System.out.printf(format, "period1.withMonths(7)", period1.withMonths(7));
+        System.out.printf(format, "period1.withDays(31)", period1.withDays(31));
     }
 }
 
