4c9e7c1ff0c6414fc4fb4e2cbbaf8139e19d9a09
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-06 12:19:56 +0000

    Add comment for ^$ usage redundancy when matches method is used to MatchesSample

--- src/main/java/com/sanver/basics/regex/MatchesSample.java
+++ src/main/java/com/sanver/basics/regex/MatchesSample.java
@@ -2,12 +2,12 @@ package com.sanver.basics.regex;
 
 public class MatchesSample {
 
-    private static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"; // The ^ and $ anchors are technically redundant since the matches() method specifically checks if the entire input sequence matches the pattern.
 
     public static void main(String... args) {
         String[] emailCandidates = {"sdfa@asd.co", "@a.com", "sd#a@asd.co", "asdf@a~a.com", "abdullah.sanver@sancode.co.uk"};
-        for (String candidate : emailCandidates)
-            System.out.printf("%-40s %s an e-mail address\n", candidate,
-                    candidate.matches(EMAIL_REGEX) ? "may be" : "is not");
+        for (String candidate : emailCandidates) {
+            System.out.printf("%-40s %s an e-mail address%n", candidate, candidate.matches(EMAIL_REGEX) ? "may be" : "is not");
+        }
     }
 }
