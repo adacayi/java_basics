ef5d96b9d455046e621048012230b72183bda12a
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-07 12:13:23 +0000

    Add new cases for PrintfSample

--- src/main/java/com/sanver/basics/console/PrintfSample.java
+++ src/main/java/com/sanver/basics/console/PrintfSample.java
@@ -71,11 +71,11 @@ package com.sanver.basics.console;
  * // Left-justify a floating-point value:
  * System.out.printf("Total: %-10.2f: ", dblTotal);
  *
- * // Pad a decimal integer with spaces:
- * System.out.printf("% 4d", intValue);
+ * // Pad a decimal integer with 0:
+ * System.out.printf("%04d", intValue);
  *
  * // Restrict string length:
- * System.out.printf("%20.10s%n", stringVal);
+ * System.out.printf("%20.10s%n", stringVal); // Prints only the first 10 characters of the stringVal and right aligns it with a width of 20 characters.
  *
  * // Print a hashcode for an object:
  * String s = "Hello World";
@@ -85,20 +85,34 @@ package com.sanver.basics.console;
 
 public class PrintfSample {
     public static void main(String[] args) {
-        double number = 6123457.32514;
+        double number = 6123457.32514; // 13 characters including the dot. 15 characters if , grouping separator is to be printed out.
         double negativeNumber = -number;
+        System.out.printf("%%f for 1.2 %9s: %f%n", "", 1.2); // If not specified, 6 digits after the decimal point will be printed out
         System.out.printf("Number %13s: %,f%n", "", number);
+        System.out.printf("%-20s: %,14f %n", "%,14f", number); // If not specified 6 digits after the decimal will be printed out, making the total length 16 characters for the number
+        System.out.printf("%-20s: %,15f %n", "%,15f", number);
+        System.out.printf("%-20s: %,16f %n", "%,16f", number);
+        System.out.printf("%-20s: %,17f %n", "%,17f", number);
         System.out.printf("%-20s: %-,15.3f %n", "%-,15.3f", number);
+        System.out.printf("%-20s: %,15.3f %n", "%,15.3f", number);
         System.out.printf("%-20s: %-,15.2f %n", "%-,15.2f", number); // Notice that half up rounding is done
         System.out.printf("%-20s: %,15.2f %n", "%,15.2f", number);
         System.out.printf("%-20s: %0,15.2f %n", "%0,15.2f", number); // forces numerical values to be zero-padded ( default is blank padding )
-        System.out.printf("%-20s: %0,20.8f %n", "%0,20.8f", number); // notice that padding is used for the integer part and the decimal part.
+        System.out.printf("%-20s: %0,20.8f %n", "%0,20.8f", number); // notice that padding is used for the integer part and the decimal part. Decimal part padding is always 0, and integer part padding here is specified as 0 with %0,20.8f as well.
+        System.out.printf("%-20s: %,20.8f %n", "%,20.8f", number); // notice that padding is used for the integer part and the decimal part. Decimal part padding is always 0, and integer part padding here is the default blank padding.
         System.out.printf("%-20s: %+,.2f %n", "%+,.2f", number);
         System.out.printf("%-20s: % ,.2f %n", "% ,.2f", number); // space will display a minus sign if the number is negative or a space if it
-        System.out.printf("%-20s: % ,.2f %n", "% ,.2f", negativeNumber);
+        System.out.printf("%-20s: % ,.2f %n", "% ,.2f for -Number", negativeNumber);
+        System.out.printf("0123456789 %%10.5s   : %10.5s%n", "0123456789"); // Prints only the first 5 characters of the string and right aligns it with a width of 10 characters.
         var obj = new PrintfSample();
         var hashCode = obj.hashCode();
-        System.out.printf("%s object.%nHashcode with %%h: %h.%nHashCode: %d.%nHashCode in hexadecimal: %x%nHashCode in octal: %o%n", obj, obj, hashCode, hashCode, hashCode); // Notice toString appends the hexadecimal hashCode to class name.
+        System.out.printf("%s object.%n", obj);// Notice toString appends the hexadecimal hashCode to class name.
+        System.out.printf("Hashcode with %%h: %h.%n", obj);
+        System.out.printf("Hashcode with %%H: %H.%n", obj);
+        System.out.printf("HashCode: %d.%n" ,hashCode);
+        System.out.printf("HashCode in hexadecimal: %x%n" , hashCode);
+        System.out.printf("HashCode in hexadecimal: %X%n" , hashCode);
+        System.out.printf("HashCode in octal: %o%n", hashCode);
         var text = "metal";
         System.out.printf("First 2 characters of \"%s\": %.2s%n", text, text);
         numberSystems();
@@ -108,7 +122,7 @@ public class PrintfSample {
         int i = 42;
         System.out.printf("%nShowing printing integers in different number systems%n");
         System.out.printf("%nDecimal: %d%n", i);
-        System.out.printf("Binary : %s%n", Integer.toBinaryString(i));
+        System.out.printf("Binary : %s%n", Integer.toString(i, 2)); // This can also be used Integer.toBinaryString(i)
         System.out.printf("Octal  : %o%n", i);
         System.out.printf("Hexadecimal (lowercase): %x%n", i);
         System.out.printf("Hexadecimal (uppercase): %X%n", i);
@@ -116,7 +130,7 @@ public class PrintfSample {
 
     @Override
     public int hashCode() {
-        return 1024;
+        return 1034;
     }
 
     @Override
