775a845f20c2bcc4457b0342b34b77cd53a76892
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-07 11:21:02 +0000

    Add comments to MoveCursorSample to show how to run it from command line and fix incorrect comments

--- src/main/java/com/sanver/basics/console/MoveCursorSample.java
+++ src/main/java/com/sanver/basics/console/MoveCursorSample.java
@@ -6,9 +6,23 @@ import static com.sanver.basics.utils.Utils.sleep;
 
 /**
  * This class is to show how to move the cursor.
- * If run in IntelliJ only \r works to move the cursor to the start of the line. \033[F and other ANSI escape characters do not move the cursor in IntelliJ.
+ * <p>If run in IntelliJ only {@code \r} works to move the cursor to the start of the line.
+ * {@code \033[F} and other ANSI escape characters do not move the cursor in IntelliJ.
  * For that, run the code from the terminal.
- * Go to \Basics\target\classes and type java com.sanver.basics.console.MoveCursorSample
+ * <h4>Ways of running from terminal</h2>
+ * <ol>
+ *     <li>Go to \Basics\target\classes and type
+ *          <pre>
+ *          {@code java com.sanver.basics.console.MoveCursorSample}
+ *          </pre>
+ *     </li>
+ *     <li> Go to \Basics and type
+ *          <pre>
+ *          {@code java -cp target/classes com.sanver.basics.console.MoveCursorSample}
+ *          </pre>
+ *     </li>
+ * </ol>
+ * </p>
  */
 public class MoveCursorSample {
     public static void main(String[] args) {
@@ -20,7 +34,7 @@ public class MoveCursorSample {
         System.out.print("Count:   ");
         // Simulate processing
         for (int i = 10; i >= 0; i--) {
-            // Move the cursor back to the end of the "Processing: " line
+            // Move the cursor back to the end of the "Count: " line
             moveLeft(2);
             System.out.printf("%2d",i);
             sleep(500);
@@ -30,7 +44,7 @@ public class MoveCursorSample {
         System.out.print("\033[F"); // The ANSI escape code \033[F moves the cursor up by one line and positions it at the beginning of that line. Use "\033[A" to just move the cursor up one line.
         moveRight( "CountDown".length() + 1);
         System.out.print("Finished"); // Overwrite the first line
-        System.out.print("\033[E"); // The ANSI escape code \033[E moves the cursor up by one line and positions it at the beginning of that line. Use "\033[B" to just move the cursor down one line.
+        System.out.print("\033[E"); // The ANSI escape code \033[E moves the cursor down by one line and positions it at the beginning of that line. Use "\033[B" to just move the cursor down one line.
         moveRight("Count:  0".length());
         System.out.println(". Done!");
     }
