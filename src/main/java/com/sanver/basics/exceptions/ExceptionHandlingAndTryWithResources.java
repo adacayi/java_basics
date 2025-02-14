43f25ba6d6a8ccf6cf098088fe69e899d5b79d5b
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-14 11:05:08 +0000

    Remove stream.read(), since exception is thrown before that in ExceptionHandlingAndTryWithResources

--- src/main/java/com/sanver/basics/exceptions/ExceptionHandlingAndTryWithResources.java
+++ src/main/java/com/sanver/basics/exceptions/ExceptionHandlingAndTryWithResources.java
@@ -8,10 +8,9 @@ public class ExceptionHandlingAndTryWithResources {
 
 	public static void main(String[] args) {
 		try (FileInputStream stream = new FileInputStream("non-existing-file.txt")) {
-			stream.read();
 		} catch (FileNotFoundException e) { // If we had this after the catch(IOException) it would result in a "FileNotFoundException has already been caught" error, because catch(IOException) would have caught it.
 			System.out.println("FileNotFoundException: " + e.getMessage());
-		} catch (IOException e1) { // This block will not be executed since stream.read() throws a FileNotFoundException in this case, and it is caught in the first catch block.
+		} catch (IOException e1) { // This block will not be executed since new FileInputStream("non-existing-file.txt") throws a FileNotFoundException in this case, and it is caught in the first catch block.
 			System.out.println("IOException: " + e1.getMessage());
 		} finally{
 			// stream.close(); // This line would give a compile error, since stream is only accessible from within the try with resources block.
