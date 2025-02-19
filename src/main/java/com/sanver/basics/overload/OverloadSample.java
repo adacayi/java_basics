3650f8f01192f2a88eed99cdceef8752be13cbd9
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-19 18:49:55 +0000

    Add cases with varargs to OverloadSample

--- src/main/java/com/sanver/basics/overload/OverloadSample.java
+++ src/main/java/com/sanver/basics/overload/OverloadSample.java
@@ -1,5 +1,7 @@
 package com.sanver.basics.overload;
 
+import java.util.Arrays;
+
 class ParentClass {
 }
 
@@ -17,6 +19,24 @@ public class OverloadSample {
     public void print(ChildClass value) {
         System.out.println("ChildClass print method");
     }
+
+//    public <T extends ChildClass> void print(T value){} // This has the same signature as the public void print(ChildClass value) and will cause a compile error
+//    public <T super ChildClass> void print(T value){} // super is not allowed here
+
+    public void print(ChildClass value, String... params) {
+        System.out.println("Printing ChildClass with parameters");
+        System.out.println(Arrays.toString(params));
+    }
+
+    public OverloadSample() {
+        System.out.println("Constructor with no parameters");
+    }
+
+    public OverloadSample(String... args) { // Constructor with varargs is allowed
+        System.out.println("Constructor with varargs");
+    }
+
+//    public OverloadSample(String[] args) {} // This has the same method signature with the above public OverloadSample(String... args) constructor
 
     public static void main(String[] args) {
         new OverloadSample().print(new GrandChild());
