981c0fbcd6fa9bfb93e546302bbe973b49cddac9
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-21 16:42:26 +0000

    Add -Infinite and division by null cases to FloatNotANumberSample

--- src/main/java/com/sanver/basics/math/FloatNotANumberSample.java
+++ src/main/java/com/sanver/basics/math/FloatNotANumberSample.java
@@ -3,14 +3,19 @@ package com.sanver.basics.math;
 public class FloatNotANumberSample {
     public static void main(String[] args) {
         float a = 6 / 0f;   // This will result in Infinity
-        // float a = 6 / 0;   // This would result in an ArithmeticException
-        float b = 0 / 0f;   // This will result in NaN
+        // float a = 6 / 0;   // This would result in an ArithmeticException since 6 / 0 is an int division which does not have an Infinite or -Infinite result.
+        float b = -1 / 0f; // This will result in -Infinity
+        float c = 0 / 0f;   // This will result in NaN
         // float b = 0 / 0;   // This will result in an ArithmeticException
-        float c = 5;
+        float d = 5;
+        Float e = null;
+        Float f = 1f;
 
         printDetails("6 / 0f", a);
-        printDetails("0 / 0f",b);
-        printDetails("5", c);
+        printDetails("-1 / 0f", b);
+        printDetails("0 / 0f",c);
+        printDetails("5", d);
+//        printDetails("1f / null", f/e); // This would result in a NullPointerException rather than a NaN
     }
 
     private static void printDetails(String operation, float a) {
