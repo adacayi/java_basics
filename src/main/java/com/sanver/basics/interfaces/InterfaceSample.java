58adf6d77fd7a9b2ea8469ae8cd4d1dd496f5c95
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-21 16:03:51 +0000

    Add comments for access modifiers to InterfaceSample and inner interface, class and abstract classes.

--- src/main/java/com/sanver/basics/interfaces/InterfaceSample.java
+++ src/main/java/com/sanver/basics/interfaces/InterfaceSample.java
@@ -6,18 +6,31 @@ interface Payable {
 
     static boolean isIncreaseRateValid(double increaseRate) {
         return increaseRate <= MAX_RATE;
-    }
+    } // This is public by default, but can be made private as well. Protected is not allowed and package private is not possible, since the default accessibility without the modifier is public.
 
     void increaseRate(double increaseRate); // This is automatically public. We cannot have method declarations (methods that don't have any bodies) with private, protected or package-private access. We can have private methods though. No protected or package-private methods are allowed.
 
     // protected void increase(); // This won't compile, because only public is allowed for method declarations.
-    // protected void increase(){} // This won't compile. Only public and private are allowed for method definitions.
+    // protected void increase(){} // This won't compile. Only public and private are allowed for method definitions and if the method definition is public, it must use default or static.
+    // public void increase(){} // This won't compile as well, since it is a public method definition (method with body), it should either be a default method or a static one.
 
     default double getMaxRate() {
         return MAX_RATE;
     }
 
     double getRate();
+
+    interface SomeInterface { // This is public and static, but not final, and cannot be made protected, package private or private.
+
+    }
+
+    class SomeClass { // This is public and static, but not final (but can be made final) and cannot be made protected, package private or private.
+
+    }
+
+    abstract class SomeAbstractClass { // This is public and static, but not final, and cannot be made protected, package private or private.
+
+    }
 }
 
 class Pay implements Payable {
