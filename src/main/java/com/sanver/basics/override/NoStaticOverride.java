ce84cf4f193f7c4c631b19a082ce18526dda96fb
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-22 13:42:05 +0000

    Add case for static method hiding where return type must be a covariant type of the hidden method to NoStaticOverride

--- src/main/java/com/sanver/basics/override/NoStaticOverride.java
+++ src/main/java/com/sanver/basics/override/NoStaticOverride.java
@@ -2,8 +2,9 @@ package com.sanver.basics.override;
 
 public class NoStaticOverride {
     static class A {
-        static void writeStatic() {
-            System.out.println("A static method");
+        static Object getType() {
+            System.out.println("Returning type for A");
+            return "A";
         }
     }
 
@@ -11,17 +12,21 @@ public class NoStaticOverride {
     }
 
     static class C extends A {
-        static void writeStatic() {
-            System.out.println("C static method");
+        static String getType() { // Note that we can use covariant return types of the hidden method `Object getType()`,
+            // but unlike hidden fields, even though technically possible,
+            // by design choice return types that are not of instance of the hidden method's return type are not allowed.
+            // i.e. static int getType(){return 1} or static void getType(){} are not allowed.
+            System.out.println("Returning type for C");
+            return "C";
         }
     }
 
     public static void main(String[] args) {
-        A.writeStatic();
-        B.writeStatic();
-        C.writeStatic();
+        A.getType();
+        B.getType();
+        C.getType();
         C c = new C();
-        c.writeStatic();
-        ((A)c).writeStatic();
+        c.getType();
+        ((A) c).getType();
     }
 }
