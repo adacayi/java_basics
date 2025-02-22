0afbecf690f31130b8a8e2ed57685d9cf8a10858
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-22 16:20:59 +0000

    Move enum inside a class to show that inner enum is always static in EnumSample

--- src/main/java/com/sanver/basics/enums/EnumSample.java
+++ src/main/java/com/sanver/basics/enums/EnumSample.java
@@ -8,87 +8,88 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/**
- * {@link Enum} implements {@link Comparable} which compares based on the ordinal value.
- * {@link Enum#compareTo}
- */
-enum Days implements Capitalize { // Note: all enum types implicitly extend the java.lang.Enum. enums cannot extend another class. e.g. enum Days extends Object is not allowed. Implements is allowed though.
-    // The first thing in an enum must be its list of constants
-    SUNDAY(7) { // This calls the private constructor of the enum.
-
-        void print() { // This overrides the print method
-            System.out.println("The day is Sunday");
-        }
-    }, // This calls the constructor with parameter 7 to generate the SUNDAY instance of the Days enum.
-    MONDAY(1) {
-        void print() {
-            System.out.println("The day is Monday");
-        }
-    },
-    TUESDAY(2) {
-        void print() {
-            System.out.println("The day is Tuesday");
-        }
-    },
-    WEDNESDAY(3) {
-        void print() {
-            System.out.println("The day is Wednesday");
-        }
-    },
-    THURSDAY(4) {
-        void print() {
-            System.out.println("The day is Thursday");
-        }
-    },
-    FRIDAY(5) {
-        void print() {
-            System.out.println("The day is Friday");
-        }
-    },
-    SATURDAY(6) {
-        void print() {
-            System.out.println("The day is Saturday");
-        }
-    };
-
-    private final int index; // You can define instance variables like normal classes
-    public String capitalizedName;
-
-    Days(int index) {  // This is a private constructor and its access modifier cannot be changed (e.g. it cannot be made public)
-        this.index = index;
-        capitalizedName = capitalize();
-    } // This is a private constructor. Enums can only have private constructors.
-
-    abstract void print(); // This is to show that enums can have abstract methods, which can be implemented in the enum constants as seen above.
-
-    public int getIndex() { // You can define methods like normal classes
-        return index;
-    }
-
-    @Override
-    public String toString() {
-        return name().charAt(0) + name().substring(1).toLowerCase(); // name is a final method in enum that returns the name as a String
-    }
-
-    @Override
-    public String capitalize() {
-        return IntStream.range(0, name().length()).mapToObj(i -> i == 0 ? String.valueOf(name().charAt(0)).toUpperCase() : String.valueOf(name().charAt(i)).toLowerCase()).collect(Collectors.joining(""));
-    }
-
-    private class SomeClass{ } // We can define inner classes, static classes abstract classes and interfaces in an enum
-
-    private static class SomeStaticClass{}
-
-    public abstract class SomeAbstractClass{}
-
-    protected interface SomeInterface{}
-}
-
-interface Capitalize {
-    String capitalize();
-}
-
 public class EnumSample {
+    interface Capitalize {
+        String capitalize();
+    }
+
+    /**
+     * {@link Enum} implements {@link Comparable} which compares based on the ordinal value.
+     * {@link Enum#compareTo}
+     */
+    enum Days implements Capitalize { // This is automatically static(i.e. no need to write `static enum Days implements Capitalize{..}`)
+        // Note: all enum types implicitly extend the java.lang.Enum. enums cannot extend another class. e.g. enum Days extends Object is not allowed. Implements is allowed though.
+        // The first thing in an enum must be its list of constants
+        SUNDAY(7) { // This calls the private constructor of the enum.
+
+            void print() { // This overrides the print method
+                System.out.println("The day is Sunday");
+            }
+        }, // This calls the constructor with parameter 7 to generate the SUNDAY instance of the Days enum.
+        MONDAY(1) {
+            void print() {
+                System.out.println("The day is Monday");
+            }
+        },
+        TUESDAY(2) {
+            void print() {
+                System.out.println("The day is Tuesday");
+            }
+        },
+        WEDNESDAY(3) {
+            void print() {
+                System.out.println("The day is Wednesday");
+            }
+        },
+        THURSDAY(4) {
+            void print() {
+                System.out.println("The day is Thursday");
+            }
+        },
+        FRIDAY(5) {
+            void print() {
+                System.out.println("The day is Friday");
+            }
+        },
+        SATURDAY(6) {
+            void print() {
+                System.out.println("The day is Saturday");
+            }
+        };
+
+        private final int index; // You can define instance variables like normal classes
+        public String capitalizedName;
+
+        Days(int index) {  // This is a private constructor and its access modifier cannot be changed (e.g. it cannot be made public)
+            this.index = index;
+            capitalizedName = capitalize();
+        } // This is a private constructor. Enums can only have private constructors.
+
+        abstract void print(); // This is to show that enums can have abstract methods, which can be implemented in the enum constants as seen above.
+
+        public int getIndex() { // You can define methods like normal classes
+            return index;
+        }
+
+        @Override
+        public String toString() {
+            return name().charAt(0) + name().substring(1).toLowerCase(); // name is a final method in enum that returns the name as a String
+        }
+
+        @Override
+        public String capitalize() {
+            return IntStream.range(0, name().length()).mapToObj(i -> i == 0 ? String.valueOf(name().charAt(0)).toUpperCase() : String.valueOf(name().charAt(i)).toLowerCase()).collect(Collectors.joining(""));
+        }
+
+        private class SomeClass{ } // We can define inner classes, static classes abstract classes and interfaces in an enum
+
+        private static class SomeStaticClass{}
+
+        public abstract class SomeAbstractClass{}
+
+        protected interface SomeInterface{}
+    }
+
     private static final Random RANDOM = new Random();
 
     public static void main(String... args) {
