9b736afe5e639c827f3d71dab439d585f287338a
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-23 12:31:48 +0000

    Add covariant return type constraint to SimpleOverrideSample

--- src/main/java/com/sanver/basics/override/SimpleOverrideSample.java
+++ src/main/java/com/sanver/basics/override/SimpleOverrideSample.java
@@ -32,6 +32,46 @@ public class SimpleOverrideSample {
 		}
 	}
 
+	static class D{
+		Number getNumber() {
+			return 1;
+		}
+
+		int getPrimitiveNumber() {
+			return 10;
+		}
+
+		static Number getStaticNumber() {
+			return 30;
+		}
+	}
+
+	static class E extends D{
+		Integer getNumber() { // Overriding method return type can only be a covariant return type of the overridden method. Boxing/unboxing, implicit conversion is not allowed.
+			return 2;
+		}
+
+		//		Object getNumber() {return new Object();} // This won't compile
+//		int getNumber() {return  1;} // This does not work.
+//		byte getPrimitiveNumber() {return 3;} // This does not work, even though byte can be implicitly converted to int.
+//		long getPrimitiveNumber() {return 3;} // This does not work.
+
+		static Integer getStaticNumber() { // There is no static method override, check NoStaticOverride,
+			// but still static methods should have the covariant return type of the static method in the base class they are hiding (not overriding).
+			return 3;
+		}
+
+//		static Object getStaticNumber() { return 3;} // This won't compile
+//		static int getStaticNumber() { return 3;} // This won't compile
+//		static byte getStaticNumber() { return 3;} // This won't compile
+//		static long getStaticNumber() { return 3;} // This won't compile
+
+	}
+
+	static class F extends E{
+//		int getNumber() {return 50;} // This does not work, although the parent method return type is Integer
+	}
+
 	/**
 	 * @param args
 	 */
