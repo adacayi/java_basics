1d4af85a90e2af341a0f7dffef1d82498c3ecee5
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-24 17:07:28 +0000

    Add comment for having inner classes, enums, records and interfaces inside an enum and record to EnumSample and RecordSample

--- src/main/java/com/sanver/basics/enums/EnumSample.java
+++ src/main/java/com/sanver/basics/enums/EnumSample.java
@@ -20,6 +20,7 @@ public class EnumSample {
     enum Days implements Capitalize { // This is automatically static(i.e. no need to write `static enum Days implements Capitalize{..}`) and cannot access any instance members of its outer class (e.g. EnumSample.this.someMember won't compile)
         // Note: all enum types implicitly extend the java.lang.Enum. enums cannot extend another class. e.g. enum Days extends Object is not allowed. Implements is allowed though.
         // The first thing in an enum must be its list of constants
+        // We can have inner classes, enums, records and interfaces inside an enum.
         SUNDAY(7) { // This calls the private constructor of the enum.
 
             void print() { // This overrides the print method
