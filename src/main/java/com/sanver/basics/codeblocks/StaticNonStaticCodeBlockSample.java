c62d1563fd33f3281add5a2341e134fa1d775f4b
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-25 17:49:54 +0000

    Add dependency to the written order of static, non-static blocks and static, non-static variables when executing the blocks and initializing the variables to StaticNonStaticCodeBlockSample

--- src/main/java/com/sanver/basics/codeblocks/StaticNonStaticCodeBlockSample.java
+++ src/main/java/com/sanver/basics/codeblocks/StaticNonStaticCodeBlockSample.java
@@ -1,13 +1,13 @@
 package com.sanver.basics.codeblocks;
 
 class A {
-    static String staticValue = "Initial"; // This line will be executed before the static block
+    static String staticValue = "Initial"; // This line will be executed before the static block, because it is written before the static block, otherwise the static block would be executed first.
 
-    static {// Static blocks are only run once, either triggered by calling a static method or a constructor of the class
+    static {// Static blocks are only run once. Look at the ClassInitialization code to see the details about what triggers running the static block.
         System.out.println("Static block A. Static value = " + staticValue);
     }
 
-    String value = "Initial non-static"; // This line will be executed before the non-static block
+    String value = "Initial non-static"; // This line will be executed before the non-static block, because it is written before the non-static block, otherwise the non-static block will be executed.
 
     {
         System.out.println("non-static block A. Value = " + value);
@@ -35,17 +35,23 @@ class B extends A {
 }
 
 class C extends B{
-    static String staticValue = "Initial C"; // This line will be executed before the static block
+    static String staticValue = "Initial C"; // This line will be executed before the static block, because it is written before the static block, otherwise the static block would be executed first.
 
     static {// Static blocks are only run once, either triggered by calling a static method or a constructor of the class
         System.out.println("Static block C. Static value = " + staticValue);
+//        System.out.println(staticValue2); // We cannot access staticValue2 at this point, because it is defined below.
     }
 
-    String value;
+    static String staticValue2 = "Static Value 2 in C";
+
+    String value; // This will be assigned to its default value before the non-static block is executed, because it is written before the non-static block, otherwise the non-static block will be executed.
 
     {
         System.out.println("non-static block C. Value = " + value);
+//        System.out.println(value2); // We cannot access value2 at this point, because it is defined below.
     }
+
+    String value2;
 
     public C(String value) {
         super(value);
