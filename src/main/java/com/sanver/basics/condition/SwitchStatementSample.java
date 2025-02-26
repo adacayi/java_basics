b34471e0595151e5d8f473e4be3d11eef79dee2b
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-26 17:06:01 +0000

    Add local variable definition and accessibility to SwitchStatementSample

--- src/main/java/com/sanver/basics/condition/SwitchStatementSample.java
+++ src/main/java/com/sanver/basics/condition/SwitchStatementSample.java
@@ -23,13 +23,18 @@ public class SwitchStatementSample {
         String result;
         switch (animal) {
             case DOG:
+               // tempResult = "Dog" // This will result in a compile error, since tempResult is not defined yet.
             case CAT: // These two cases can be combined into one case as shown in the below getAnimalType() method.
+                String tempResult = "cat"; // tempResult is defined within the scope of the switch statement, thus can be accessed within the case CAT: block and any subsequent case blocks after it.
                 result = "domestic animal";
                 break;
             case TIGER:
-                result = "wild animal";
+//                System.out.println(tempResult); // This results in a compile error since, tempResult is not initialized. Even if we didn't have the break on the above case and there is a fall through, this won't compile.
+                tempResult = "wild animal"; // This does not cause a compile error since tempResult is in the scope for the rest of the switch statement
+                result = tempResult;
                 break;
             default: // This does not need to be the last case, unlike the switch pattern matching scenario. Assume it is the first case, then it will be executed if animal does not match any other cases.
+                tempResult = "";  // This does not cause a compile error since tempResult is in the scope for the rest of the switch statement
                 result = "unknown animal";
                 break;
         }
