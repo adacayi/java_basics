5ecbfdac33d102eed53816052963f0717b0f861d
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-26 18:35:18 +0000

    Add non-static method overriding in InterfaceSample

--- src/main/java/com/sanver/basics/interfaces/InterfaceSample.java
+++ src/main/java/com/sanver/basics/interfaces/InterfaceSample.java
@@ -83,6 +83,13 @@ public class InterfaceSample {
         System.out.println("B.value2: " + B.value2); // Unlike static methods, we can access static fields of an interface through the implementing class or extending interface
 //        System.out.println(new C().value); // This will result in a compile error though, since reference to 'value' is ambiguous, both 'A. value' and 'B. value' match
         System.out.println("B.value: " + B.value); // This will work fine, since B.value hides A.value
+        var c = new C();
+        c.process();
+        c.process2();
+        ((B) c).process2();
+        c.process3();
+        c.process4();
+        c.process5();
     }
 
     interface A {
@@ -96,6 +103,24 @@ public class InterfaceSample {
         static void print() {
             System.out.println("A print");
         }
+
+        static void process() {
+            System.out.println("Processing a");
+        }
+
+        static void process2() {
+            System.out.println("Processing a 2");
+        }
+
+        void process3();
+
+        default void process4() {
+            System.out.println("Processing A 4");
+        }
+
+        private void process5() {
+            System.out.println("Processing A 5");
+        }
     }
 
     interface B extends A {
@@ -104,9 +129,47 @@ public class InterfaceSample {
         static void print() {
             System.out.println("B print");
         }
+
+        void process(); // Note that we can have a non-static method with same name in the sub-interface
+        default void process2(){ // Note that we can have a default method with same name in the sub-interface
+            System.out.println("Processing B 2");
+        }
+
+//        static void process3() { // Overriding a non-static method with a static method is not allowed.
+//            System.out.println("Processing B 3");
+//        }
+
+//        static void process4() { }// This is not allowed as well.
+
+        void process4(); // This is allowed.
+
+//         private void process4(); // This is not allowed, since although an interface can have private methods, you still cannot override a public method with a private method.
+
+        void process5(); // This is allowed.
+//        default void process5() { // This is allowed as well.
+//            System.out.println("Processing B 5");
+//        }
     }
 
     static class C implements A, B {
+        public void process() {
+            System.out.println("Processing C");
+        }
 
+        public void process2() { // Note that this overrides process2 in B
+            System.out.println("Processing C 2");
+        }
+
+        public void process3() {
+            System.out.println("Processing C 3");
+        }
+
+        public void process4() {
+            System.out.println("Processing C 4");
+        }
+
+        public void process5() {
+            System.out.println("Processing C 5");
+        }
     }
 }
