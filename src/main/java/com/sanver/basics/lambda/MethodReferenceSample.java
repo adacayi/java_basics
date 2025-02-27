1043022a57212ee01252200477711696eaad3923
Abdullah Sanver <a_sanver@hotmail.com>
2025-02-27 13:32:40 +0000

    Change MethodReferenceSample to show different method reference cases

--- src/main/java/com/sanver/basics/lambda/MethodReferenceSample.java
+++ src/main/java/com/sanver/basics/lambda/MethodReferenceSample.java
@@ -1,24 +1,56 @@
 package com.sanver.basics.lambda;
 
-import java.awt.event.ActionEvent;
+import org.springframework.expression.spel.ast.MethodReference;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class MethodReferenceSample {
+    private final String name;
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Method Reference Sample");
-		JButton button = new JButton("Click me..");
-		button.addActionListener(new MethodReferenceSample()::showDialog);
-		frame.add(button);
-		frame.setSize(300, 200);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
+    public MethodReferenceSample(String name) {
+        this.name = name;
+    }
 
-	public void showDialog(ActionEvent event) {
-		JOptionPane.showConfirmDialog(null, "Pressed", "Information", JOptionPane.DEFAULT_OPTION);
-	}
+    public static void main(String[] args) {
+        new MethodReferenceSample("1").print();
+    }
+
+    public static String getStaticString() {
+        return "Static String";
+    }
+
+    public void print() {
+        var text = "Text";
+        Supplier<String> s = text::toUpperCase; // this refers to text.toUpperCase()
+        System.out.println(s.get());
+        s = this::getString; // this refers to this.getString()
+        System.out.println(s.get());
+        s = MethodReferenceSample::getStaticString; // this refers to MethodReferenceSample.getStaticString()
+        System.out.println(s.get());
+//        s = this::getStaticString; // This will result in a compile error, because a static method referenced through non-static qualifier
+        Function<MethodReferenceSample, String> getName = MethodReferenceSample::getName2; // this refers to a static method in MethodReferenceSample static String getName2(MethodReferenceSample)
+        // or a non-static method in MethodReferenceSample String getName2().
+        // If both are present in MethodReferenceSample, it will result in a compile error.
+        // Comment out the static getName2 to see the compile error.
+        System.out.println(getName.apply(this));
+        getName = this::getName; // This refers to String getName(MethodReferenceSample)
+        System.out.println(getName.apply(this));
+    }
+
+//    public static String getName2(MethodReferenceSample obj) {
+//        return "static ";
+//    }
+
+    public String getName2() {
+        return "name2: " + name;
+    }
+
+    public String getName(MethodReferenceSample obj) {
+        return "name: " + obj.name;
+    }
+
+    public String getString() {
+        return "non - static string";
+    }
 }
