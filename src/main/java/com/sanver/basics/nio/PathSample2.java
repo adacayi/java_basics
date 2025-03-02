df314a1943a2ba46d1d18a05ba57f54ac00146fe
Abdullah Sanver <a_sanver@hotmail.com>
2025-03-02 11:05:12 +0000

    Add relativize case for between normalized and non-normalized paths to PathSample2

--- src/main/java/com/sanver/basics/nio/PathSample2.java
+++ src/main/java/com/sanver/basics/nio/PathSample2.java
@@ -10,6 +10,8 @@ public class PathSample2 {
         var path4 = Path.of("folder5\\other2.txt"); // This is a relative path
         var path5 = Path.of("folder5\\folder6\\other3.txt"); // This is a relative path
         var path6 = Path.of("\\folder1\\folder2\\.\\folder3\\..\\some2.txt"); // When resolved this equals to \\folder1\folder2\some2.txt, since `.` means current directory and `..` means parent directory.
+        var path7 = Path.of("folder1\\folder2\\.\\folder3\\..\\some3.txt");
+        var path8 = Path.of("folder1\\folder2\\.\\folder3\\folder4\\..\\some4.txt");
         var format = "%-38s : %s%n";
         System.out.printf(format, "path1", path1);
         System.out.printf(format, "path2", path2);
@@ -18,6 +20,11 @@ public class PathSample2 {
         System.out.printf(format, "path5", path5);
         System.out.printf(format, "path6", path6);
         System.out.printf(format, "path6.normalize()", path6.normalize());
+        System.out.printf(format, "path7", path7);
+        System.out.printf(format, "path7.normalize()", path7.normalize());
+        System.out.printf(format, "path8", path8);
+        System.out.printf(format, "path8.normalize()", path8.normalize());
+
         System.out.println();
 
         System.out.printf(format, "path4.resolve(path5)", path4.resolve(path5));
@@ -27,6 +34,13 @@ public class PathSample2 {
         System.out.printf(format, "path1.resolve(path4)", path1.resolve(path4));
         System.out.printf(format, "path3.resolve(path4)", path3.resolve(path4));
         System.out.printf(format, "path6.resolve(path4)", path6.resolve(path4));
+        System.out.printf(format, "path6.resolve(path6.normalize())", path6.resolve(path6.normalize()));
+        System.out.printf(format, "path6.normalize().resolve(path6)", path6.normalize().resolve(path6));
+        System.out.printf(format, "path7.resolve(path7.normalize())", path7.resolve(path7.normalize()));
+        System.out.printf(format, "path7.normalize().resolve(path7)", path7.normalize().resolve(path7));
+        System.out.printf(format, "path7.resolve(path8)", path7.resolve(path8));
+        System.out.printf(format, "path7.relativize(path7.resolve(path8))", path7.relativize(path7.resolve(path8))); // This is equal to path8.normalize(), because relativize() normalizes the paths before relativizing
+        System.out.println();
 
         System.out.printf(format, "path1.resolve(path2)", path1.resolve(path2));
         System.out.printf(format, "path2.resolve(path1)", path2.resolve(path1));
@@ -40,7 +54,12 @@ public class PathSample2 {
 //        System.out.printf(format, "path1.relativize(path3)", path1.relativize(path3)); // this would result in an exception, although both path1 and path3 is absolute, path1.isAbsolute results in false
         System.out.printf(format, "path1.relativize(path6)", path1.relativize(path6));
         System.out.printf(format, "path6.relativize(path1)", path6.relativize(path1));
+        System.out.printf(format, "path6.relativize(path1).getNameCount()", path6.relativize(path1).getNameCount());
         System.out.printf(format, "path3.resolve(path1).relativize(path3)", path3.resolve(path1).relativize(path3));
         System.out.printf(format, "path3.resolve(path1).relativize(path3).isAbsolute()", path3.resolve(path1).relativize(path3).isAbsolute());
+        System.out.printf(format, "path6.relativize(path6.normalize())", path6.relativize(path6.normalize()));
+        System.out.printf(format, "path6.normalize().relativize(path6)", path6.normalize().relativize(path6));
+        System.out.printf(format, "path6.relativize(path6).getNameCount()", path6.relativize(path6).getNameCount());
+        System.out.printf(format, "path6.relativize(path6).getName(0)", path6.relativize(path6).getName(0));
     }
 }
