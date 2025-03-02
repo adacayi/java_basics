9b1b4a1965a8dbbdb5d43cb1283cd9804072b9b7
Abdullah Sanver <a_sanver@hotmail.com>
2025-03-02 14:05:46 +0000

    Add comments for the exception cases for FileReader and FileWriter to FileWriterFileReaderSample

--- src/main/java/com/sanver/basics/io/streams/FileWriterFileReaderSample.java
+++ src/main/java/com/sanver/basics/io/streams/FileWriterFileReaderSample.java
@@ -9,20 +9,26 @@ public class FileWriterFileReaderSample {
 
     public static void main(String[] args) {
         String fileName = "src/main/java/com/sanver/basics/io/streams/FileWriterReaderDemo.txt";
-        var file = new File(fileName);
+        var file = new File(fileName); // Note that file is not created here, but when new FileWriter(fileName) is called.
         file.deleteOnExit();
         // Writing to the file
-        try (FileWriter writer = new FileWriter(fileName)) {
+        try (FileWriter writer = new FileWriter(fileName)) { // If file does not exist, it will be created.
+            // We can also use new FileWriter(file).
+            // new FileWriter(file, true) can be used for appending to an existing file (A new file will be created if the file does not exist).
+            // It will throw an IOException if the named file exists but is a directory rather than a regular file,
+            // does not exist but cannot be created, or cannot be opened for any other reason.
             writer.write("This is a demonstration of FileWriter.\n");
-            writer.append("FileWriter writes character streams easily.\n")
-                    .append("This is the last line.\n");
+            writer.append("FileWriter writes character streams easily.\n").append("This is the last line.\n"); // append behaves in exactly the same way as the invocation write(), but also returns back the writer.
+            // To actually append to the end of a file, use the constructor new FileWriter(file, true)
             writer.flush();
         } catch (IOException e) {
             System.out.println("Error while writing to the file: " + e.getMessage());
         }
 
         // Reading from the file
-        try (FileReader reader = new FileReader(fileName)) {
+        try (FileReader reader = new FileReader(fileName)) { // We can also use new FileReader(file).
+            // It will throw an FileNotFoundException if the named file does not exist,
+            // is a directory rather than a regular file, or for some other reason cannot be opened for reading.
             int character;
             while ((character = reader.read()) != -1) {
                 System.out.print((char) character);
