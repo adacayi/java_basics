b4f883e25f51dfcaf90816edcb5f8e26004cebad
Abdullah Sanver <a_sanver@hotmail.com>
2025-03-03 13:08:24 +0000

    Add ambiguity cases for overload to OverloadSample

--- src/main/java/com/sanver/basics/overload/OverloadSample.java
+++ src/main/java/com/sanver/basics/overload/OverloadSample.java
@@ -2,32 +2,7 @@ package com.sanver.basics.overload;
 
 import java.util.Arrays;
 
-class ParentClass {
-}
-
-class ChildClass extends ParentClass {
-}
-
-class GrandChild extends ChildClass {
-}
-
 public class OverloadSample {
-    public void print(ParentClass value) {
-        System.out.println("ParentClass print method");
-    }
-
-    public void print(ChildClass value) {
-        System.out.println("ChildClass print method");
-    }
-
-//    public <T extends ChildClass> void print(T value){} // This has the same signature as the public void print(ChildClass value) and will cause a compile error
-//    public <T super ChildClass> void print(T value){} // super is not allowed here
-
-    public void print(ChildClass value, String... params) {
-        System.out.println("Printing ChildClass with parameters");
-        System.out.println(Arrays.toString(params));
-    }
-
     public OverloadSample() {
         System.out.println("Constructor with no parameters");
     }
@@ -36,9 +11,62 @@ public class OverloadSample {
         System.out.println("Constructor with varargs");
     }
 
-//    public OverloadSample(String[] args) {} // This has the same method signature with the above public OverloadSample(String... args) constructor
+    //    public OverloadSample(String[] args) {} // This has the same method signature with the above public OverloadSample(String... args) constructor
 
     public static void main(String[] args) {
-        new OverloadSample().print(new GrandChild());
+        var overloadSample = new OverloadSample();
+        overloadSample.print(new C());
+        overloadSample.print(new B());
+//        overloadSample.print(new B(), new B()); // This will result in a compile error because there are two methods with print(A a, B b) and print(B b, A a), that are applicable and accessible, and neither is more specific than the other, causing an ambiguity.
+//        overloadSample.print(new C(), new C()); // This will result in a compile error, for the same reason as above.
+        overloadSample.print(new B(), new B(), new C()); // This works even though there are two methods print(B b1, B b2, A a) and print(B b, A a1, A a2), that are applicable and accessible, print(B b1, B b2, A a) is more specific.
+    }
+
+    void print(A value) {
+        System.out.println("A printed. " + value);
+    }
+
+    void print(B value) {
+        System.out.println("B printed. " + value);
+    }
+//    public <T extends B> void print(T value){} // This has the same signature as the public void print(B value) and will cause a compile error
+//    public <T super B> void print(T value){} // super is not allowed here
+
+    void print(C value) {
+        System.out.println("C printed. " + value);
+    }
+
+    void print(B value, String... params) {
+        System.out.println("Printing B with parameters");
+        System.out.println(value + " " + Arrays.toString(params));
+    }
+
+    void print(A a, B b) {
+        System.out.printf("A: %s B: %s%n", a, b);
+    }
+
+    void print(B b, A a) {
+        System.out.printf("B: %s A: %s%n", b, a);
+    }
+
+    void print(B b1, B b2, A a) {
+        System.out.printf("B1: %s B2: %s A: %s%n", b1, b2, a);
+    }
+
+    void print(B b, A a1, A a2) {
+        System.out.printf("B: %s A1: %s A2: %s%n", b, a1, a2);
+    }
+
+    static class A {
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+    }
+
+    static class B extends A {
+    }
+
+    static class C extends B {
     }
 }
