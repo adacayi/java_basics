956e56051c3baeaa57c2859e631eb40912f19dec
Abdullah Sanver <a_sanver@hotmail.com>
2025-03-11 10:51:35 +0000

    Add different ways to generate random numbers to RandomStream

--- src/main/java/com/sanver/basics/streamapi/RandomStream.java
+++ src/main/java/com/sanver/basics/streamapi/RandomStream.java
@@ -2,11 +2,23 @@ package com.sanver.basics.streamapi;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class RandomStream {
     public static void main(String[] args) {
         var random = new Random();
-        var randomNumberStream = random.ints(10, 1, 11); // Generates an int stream of size 10, with random integers between 1 and 10
+        IntStream randomNumberStream = random.ints(10, 1, 11); // Generates an int stream of size 10, with random integers between 1 and 10
         System.out.println(Arrays.toString(randomNumberStream.toArray()));
+
+        DoubleStream doubleStream = random.doubles(10);
+        System.out.println(doubleStream.boxed().toList());
+
+        doubleStream = random.doubles().limit(10);
+        System.out.println(doubleStream.boxed().toList());
+
+
+        doubleStream = DoubleStream.generate(random::nextDouble).limit(10);
+        System.out.println(doubleStream.boxed().toList());
     }
 }
